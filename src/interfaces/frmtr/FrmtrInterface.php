32f432984ba7a3bcb421fe29152173c9f68b2269
dougw <dougwilbourne@gmail.com>
2022-09-04 09:11:04 -0400

    added formatter interface getters

--- src/interfaces/frmtr/FrmtrInterface.php
+++ src/interfaces/frmtr/FrmtrInterface.php
@@ -13,5 +13,9 @@ interface FrmtrInterface
 {
     public function setFormat(string $format) : void;
 
+    public function getFormat() : string;
+
     public function setLocale(Locale $locale) : void;
+
+    public function getLocale() : Locale;
 }
