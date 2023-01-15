1b90a81cd38ceed2752f4d19d7b6e557afe96388
dougw <dougwilbourne@gmail.com>
2023-01-15 14:27:48 -0500

    added NodeType as generic type to parameters / returns in TreenodeOrderedInterface

--- src/interfaces/struct/tree/node/TreenodeOrderedInterface.php
+++ src/interfaces/struct/tree/node/TreenodeOrderedInterface.php
@@ -34,51 +34,51 @@ interface TreenodeOrderedInterface extends TreenodeInterface
 
     /**
      * @function setReferences
-     * @param TreeOrderedInterface<NodeValueType> $tree
+     * @param TreeOrderedInterface<NodeValueType, NodeType> $tree
      */
     public function setReferences(TreeOrderedInterface $tree): void;
 
     /**
      * @function getTree
-     * @return TreeOrderedInterface<NodeValueType>|null
+     * @return TreeOrderedInterface<NodeValueType, NodeType>|null
      */
     public function getTree(): ?TreeOrderedInterface;
 
 	/**
 	 * setChildList
-	 * @param ListOrderedInterface<TreenodeOrderedInterface<NodeValueType>> $list
+	 * @param ListOrderedInterface<TreenodeOrderedInterface<NodeValueType, NodeType>> $list
 	 */
 	public function setChildList(ListOrderedInterface $list) : void;
 
 	/**
 	 * @function getChildren
-	 * @return ListOrderedInterface<TreenodeOrderedInterface<NodeValueType>>
+	 * @return ListOrderedInterface<TreenodeOrderedInterface<NodeValueType, NodeType>>
 	 */
 	public function getChildren(): ListOrderedInterface;
 
 
 	/**
      * @function getParent
-     * @return TreenodeOrderedInterface<NodeValueType>|null
+     * @return TreenodeOrderedInterface<NodeValueType, NodeType>|null
      */
     public function getParent(): ?TreenodeOrderedInterface;
 
     /**
      * @function getChild
      * @param int $nodeid
-     * @return TreenodeOrderedInterface<NodeValueType>|null
+     * @return TreenodeOrderedInterface<NodeValueType, NodeType>|null
      */
     public function getChild(int $nodeid): ?TreenodeOrderedInterface;
 
     /**
      * @function getChildrenArray
-     * @return TreenodeOrderedInterface<NodeValueType>[]
+     * @return TreenodeOrderedInterface<NodeValueType, NodeType>[]
      */
     public function getChildrenArray(): array;
 
     /**
      * @function getSiblings
-     * @return ListOrderedInterface<TreenodeOrderedInterface<NodeValueType>>|null
+     * @return ListOrderedInterface<TreenodeOrderedInterface<NodeValueType, NodeType>>|null
      */
     public function getSiblings(): ?ListOrderedInterface;
 
@@ -102,14 +102,14 @@ interface TreenodeOrderedInterface extends TreenodeInterface
 
     /**
      * @function isDescendantOf
-     * @param TreenodeOrderedInterface<NodeValueType> $node
+     * @param TreenodeOrderedInterface<NodeValueType, NodeType> $node
      * @return bool
      */
     public function isDescendantOf(TreenodeOrderedInterface $node): bool;
 
     /**
      * @function isAncestorOf
-     * @param TreenodeOrderedInterface<NodeValueType> $node
+     * @param TreenodeOrderedInterface<NodeValueType, NodeType> $node
      * @return bool
      */
     public function isAncestorOf(TreenodeOrderedInterface $node): bool;
