25407bed3af7122d9c482e24fbc326e34ab87d11
dougw <dougwilbourne@gmail.com>
2023-02-15 22:13:49 -0500

    improving DomainCatalogLoaderInterface

--- src/msg/DomainCatalogLoaderInterface.php
+++ src/msg/DomainCatalogLoaderInterface.php
@@ -17,24 +17,8 @@ interface DomainCatalogLoaderInterface
      * @function loadCatalog
      * @param string $domain
      * @param string $locale
+     * @return array<string, string>
      */
-	public function loadCatalog(string $domain, string $locale): void;
+	public function loadCatalog(string $domain, string $locale): array;
 
-	/**
-	 * @function getDomain
-	 * @return string
-	 */
-	public function getDomain(): string;
-
-	/**
-	 * @function getLocale
-	 * @return string
-	 */
-	public function getLocale(): string;
-
-	/**
-	 * @function getMessages
-	 * @return array<string>
-	 */
-	public function getMessages(): array;
 }
