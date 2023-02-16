230491cb340e87ef501d771711d3014647c12b46
dougw <dougwilbourne@gmail.com>
2023-02-16 11:40:33 -0500

    implementing fallback strategy for catalogs

--- src/msg/DomainCatalogLoaderInterface.php
+++ src/msg/DomainCatalogLoaderInterface.php
@@ -18,8 +18,8 @@ interface DomainCatalogLoaderInterface
      * @function loadCatalog
      * @param string $domain
      * @param string $locale
-     * @return array<string, string>
+     * @return false|array<string, string>
      */
-	public function loadCatalog(string $domain, string $locale): array;
+	public function loadCatalog(string $domain, string $locale): bool|array;
 
 }
