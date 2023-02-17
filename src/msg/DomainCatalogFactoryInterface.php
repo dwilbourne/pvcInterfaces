51766e8838940d6361de7e58944ae4710f67bb02
dougw <dougwilbourne@gmail.com>
2023-02-17 16:21:27 -0500

    renamed factory

--- src/msg/DomainCatalogFactoryInterface.php
+++ src/msg/DomainCatalogFactoryInterface.php
@@ -8,7 +8,7 @@ declare (strict_types=1);
 
 namespace pvc\interfaces\msg;
 
-interface DomainCatalogLoaderFactoryInterface
+interface DomainCatalogFactoryInterface
 {
     public function create(string $domain): DomainCatalogLoaderInterface;
 }
