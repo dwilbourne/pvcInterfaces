0c95bccb3716f769f18890589e7d56bedc4d239e
dougw <dougwilbourne@gmail.com>
2023-04-20 09:41:59 -0400

    added HasTxtMsgInterface

---
+++ src/txtmsg/HasTxtMsgInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * @package pvcInterfaces
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare (strict_types=1);
+
+namespace pvc\interfaces\txtmsg;
+
+interface HasTxtMsgInterface
+{
+    public function getTxtMsg() : string;
+}
