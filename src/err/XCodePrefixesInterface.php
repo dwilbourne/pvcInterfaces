5e7ef6f0e9ed152376d545d200410b1a3e811e14
dougw <dougwilbourne@gmail.com>
2023-07-26 10:43:25 -0400

    updated XDataInterface by adding method getXMessageVariables and removing countXMessageVariables.

--- src/err/XCodePrefixesInterface.php
+++ src/err/XCodePrefixesInterface.php
@@ -9,18 +9,30 @@ declare (strict_types=1);
 
 namespace pvc\interfaces\err;
 
+/**
+ * Interface XCodePrefixesInterface
+ * @package pvc\interfaces\err
+ */
 interface XCodePrefixesInterface
 {
     /**
-     * @function getXCodePrefix
+     * @function getXCodePrefix returns the "prefix" corresponding to an exception library.
+     *
+     * Prefixes are what guarantee the uniqueness of exception codes *between* exception libraries.  "Local" exception
+     * codes are guaranteed to be unique within an exception library.  Prepending the prefix to a local exception
+     * code when generating the exception guarantees its global uniqueness.
+     *
      * @param string $namespace
      * @return int
      */
     public static function getXCodePrefix(string $namespace) : int;
 
     /**
-     * @function getXCodePrefixes
-     * @return array<string, int>
+     * @function getXCodePrefixes returns an array of exception code prefixes.
+     *
+     * The key in the array is the namespace of the exception library.
+     *
+     * @return array<class-string, int>
      */
     public static function getXCodePrefixes() : array;
 }
--- src/err/XDataInterface.php
+++ src/err/XDataInterface.php
@@ -1,45 +1,62 @@
 <?php
 
 /**
- * @package pvcInterfaces
  * @author: Doug Wilbourne (dougwilbourne@gmail.com)
  */
 declare (strict_types=1);
 
 namespace pvc\interfaces\err;
 
+/**
+ * Interface XDataInterface is the interface for all Exception Data (XData) Classes.
+ *
+ * Concrete XData classes are always child classes the XDataAbstract class, which implements most of the code needed to
+ * support this interface.  Concrete classes need to implement only the two abstract methods in XDataAbstract:
+ * getLocalXCodes and getMessageTemplates.
+ *
+ * @package pvc\interfaces\err
+ */
 interface XDataInterface
 {
     /**
-     * @function getLocalXCodes
+     * @function getLocalXCodes returns an array of exception codes.
+     *
+     * The key for each code is the class string of the exception to which the code corresponds.
+     *
      * @return array<class-string, int>
      */
     public function getLocalXCodes() : array;
 
     /**
-     * @function getLocalXCode
+     * @function getLocalXCode returns the exception code for a particular exception.
      * @param class-string $classString
      * @return int
      */
     public function getLocalXCode(string $classString) : int;
 
     /**
-     * @function getXMessageTemplates
+     * @function getXMessageTemplates returns an array of exception message templates.
+     *
+     * The key for each template is the class string of the exception to which the message template belongs.  Messages
+     * can be parameterized with variables that can be populated with data available at runtime.  See the Discussion
+     * section of http://pvcerr.readthedocs.io/en/latest for a full explanation of the syntax and usage.
+     *
      * @return array<class-string, string>
      */
     public function getXMessageTemplates() : array;
 
     /**
-     * @function getXMessageTemplate
+     * @function getXMessageTemplate returns the message template corresponding to a particular exception.
      * @param class-string $classString
      * @return string
      */
     public function getXMessageTemplate(string $classString) : string;
 
     /**
-     * @function countXMessageVariables
+     * @function getXMessageVariables returns an array (possibly empty) of the variables in a message template.
+     *
      * @param string $messageTemplate
-     * @return int
+     * @return array
      */
-    public function countXMessageVariables(string $messageTemplate): int;
+    public function getXMessageVariables(string $messageTemplate): array;
 }
