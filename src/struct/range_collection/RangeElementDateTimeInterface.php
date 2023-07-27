9104254dc7ea2bde513035002266cd66b6746f99
dougw <dougwilbourne@gmail.com>
2023-07-27 15:31:56 -0400

    updated RangeElementDateTimeInterface to include DateTime|DateTimeImmutable

--- src/struct/range_collection/RangeElementDateTimeInterface.php
+++ src/struct/range_collection/RangeElementDateTimeInterface.php
@@ -9,25 +9,26 @@ declare(strict_types=1);
 namespace pvc\interfaces\struct\range_collection;
 
 use DateTime;
+use DateTimeImmutable;
 
 /**
  * @interface RangeElementDateTimeInterface
- * @extends RangeElementInterface<RangeElementDateTimeInterface, DateTime>
+ * @extends RangeElementInterface<RangeElementDateTimeInterface, DateTime|DateTimeImmutable>
  */
 interface RangeElementDateTimeInterface extends RangeElementInterface
 {
     /**
-     * @return array<DateTime>
+     * @return array<DateTime|DateTimeImmutable>
      */
     public function getRange(): array;
 
     /**
-     * @return DateTime
+     * @return DateTime|DateTimeImmutable
      */
-    public function getMin(): DateTime;
+    public function getMin(): DateTime|DateTimeImmutable;
 
     /**
-     * @return DateTime
+     * @return DateTime|DateTimeImmutable
      */
-    public function getMax(): DateTime;
+    public function getMax(): DateTime|DateTimeImmutable;
 }
