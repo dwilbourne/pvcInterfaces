ef2f76c78c8991e2b09b295a86441c469d009ccf
dougw <dougwilbourne@gmail.com>
2024-01-29 11:06:40 -0500

    changed MsgConfigInterface so there are no dependencies on the Msg library

--- src/config/MsgConfigInterface.php
+++ src/config/MsgConfigInterface.php
@@ -8,17 +8,15 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\config;
 
-use pvc\interfaces\msg\DomainCatalogLoaderInterface;
-
 /**
  * Interface MsgConfigInterface
  */
 interface MsgConfigInterface
 {
     /**
-     * getDomainCatalogLoader
+     * getDomainCatalogConfig
      * @param string $domain
-     * @return DomainCatalogLoaderInterface
+     * @return array
      */
-    public static function getDomainCatalogLoader(string $domain): DomainCatalogLoaderInterface;
+    public static function getDomainCatalogConfig(string $domain): array;
 }
