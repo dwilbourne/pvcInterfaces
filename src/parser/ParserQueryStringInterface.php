c8dece531d444d2dac0a3670d980291f3e1ad30f
dougw <dougwilbourne@gmail.com>
2024-02-12 13:42:22 -0500

    added setter and getter for QueryString object inside ParserQueryStringInterface.php

--- src/parser/ParserQueryStringInterface.php
+++ src/parser/ParserQueryStringInterface.php
@@ -8,9 +8,22 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\parser;
 
+use pvc\interfaces\http\QueryStringInterface;
+
 /**
  * Class ParserQueryStringInterface
  */
 interface ParserQueryStringInterface extends ParserInterface
 {
+    /**
+     * setQueryString
+     * @param QueryStringInterface $queryString
+     */
+    public function setQueryString(QueryStringInterface $queryString): void;
+
+    /**
+     * getQueryString
+     * @return QueryStringInterface
+     */
+    public function getQueryString(): QueryStringInterface;
 }
