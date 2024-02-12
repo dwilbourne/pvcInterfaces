9a0499992e4355830b36d4f3ee9b2e13dd2420c2
dougw <dougwilbourne@gmail.com>
2024-02-12 15:00:03 -0500

    updated UrlInterface so that all getters return strings

--- src/http/UrlInterface.php
+++ src/http/UrlInterface.php
@@ -3,6 +3,7 @@
 /**
  * @author: Doug Wilbourne (dougwilbourne@gmail.com)
  */
+
 declare(strict_types=1);
 
 namespace pvc\interfaces\http;
@@ -14,23 +15,23 @@ interface UrlInterface
 {
     public function setScheme(string $scheme): void;
 
-    public function getScheme(): ?string;
+    public function getScheme(): string;
 
     public function setHost(string $host): void;
 
-    public function getHost(): ?string;
+    public function getHost(): string;
 
     public function setPort(int|string $port): void;
 
-    public function getPort(): ?int;
+    public function getPort(): string;
 
     public function setUser(string $user): void;
 
-    public function getUser(): ?string;
+    public function getUser(): string;
 
     public function setPassword(string $pwd): void;
 
-    public function getPassword(): ?string;
+    public function getPassword(): string;
 
     public function setPath(string $path): void;
 
@@ -38,11 +39,13 @@ interface UrlInterface
 
     public function setQuery(string $queryString): void;
 
+    public function getQuery(): string;
+
     public function setFragment(string $fragment): void;
 
-    public function getFragment(): ?string;
+    public function getFragment(): string;
 
     public function setAttributesFromArray(array $urlParts): void;
 
-    public function generateURLString(): string;
+    public function generateURLString(bool $encoded = true): string;
 }
