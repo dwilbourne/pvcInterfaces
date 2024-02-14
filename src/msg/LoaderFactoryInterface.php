fe2873346485201f00144bb8f4d3a5391546857c
dougw <dougwilbourne@gmail.com>
2024-02-14 14:39:46 -0500

    added LoaderFactoryInterface.php

---
+++ src/msg/LoaderFactoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace pvc\interfaces\msg;
+
+/**
+ * Class LoaderFactoryInterface
+ */
+interface LoaderFactoryInterface
+{
+    /**
+     * makeLoader
+     * @param string $loaderType
+     * @param array $parameters <string, string>
+     * @return LoaderFactoryInterface
+     */
+    public function makeLoader(string $loaderType, array $parameters): LoaderFactoryInterface;
+}
