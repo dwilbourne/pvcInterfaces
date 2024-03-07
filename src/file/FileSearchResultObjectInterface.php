832b84c0d75264d8b9f8188e25f5ce9408ffde4b
Doug Wilbourne <dougwilbourne@gmail.com>
2024-03-07 11:31:38 -0500

    added optional dirName argument to the initialize method of FileSearchResultObjectInterface

--- src/file/FileSearchResultObjectInterface.php
+++ src/file/FileSearchResultObjectInterface.php
@@ -15,8 +15,11 @@ interface FileSearchResultObjectInterface
 {
     /**
      * initialize
+     * @param string|null $dirName
+     *
      * resets the object so that it may be used in successive file searches.  This method is called at the beginning
-     * of each file search.
+     * of each file search. The $dirName argument would typically be used if you want to include the start directory
+     * as part of the result set
      */
-    public function initialize(): void;
+    public function initialize(string $dirName = null): void;
 }
