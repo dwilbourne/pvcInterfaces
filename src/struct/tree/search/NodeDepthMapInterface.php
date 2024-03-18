8d016f6cdeefd3ebf37c9ae30e7fd3f49ec68235
Doug Wilbourne <dougwilbourne@gmail.com>
2024-03-18 18:30:09 -0400

    added NodeDepthMapInterface

---
+++ src/struct/tree/search/NodeDepthMapInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace pvc\interfaces\struct\tree\search;
+
+/**
+ * Class NodeDepthMapInterface
+ */
+interface NodeDepthMapInterface
+{
+    /**
+     * initialize
+     */
+    public function initialize(): void;
+
+    /**
+     * isEmpty
+     * @return bool
+     */
+    public function isEmpty(): bool;
+
+    /**
+     * getNodeDepth
+     * @param non-negative-int $nodeId
+     * @return non-negative-int|null
+     */
+    public function getNodeDepth(int $nodeId): ?int;
+
+    /**
+     * setNodeDepth
+     * @param non-negative-int $nodeId
+     * @param non-negative-int $depth
+     */
+    public function setNodeDepth(int $nodeId, int $depth): void;
+}
