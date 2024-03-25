e6ac517eff3e97e6bb59f283ba37648665cd4bba
Doug Wilbourne <dougwilbourne@gmail.com>
2024-03-25 15:53:46 -0400

    added getCollectionFactory to TreenodeFactoryInterface

--- src/struct/tree/node/factory/TreenodeFactoryInterface.php
+++ src/struct/tree/node/factory/TreenodeFactoryInterface.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace pvc\interfaces\struct\tree\node\factory;
 
 use pvc\interfaces\struct\collection\CollectionAbstractInterface;
+use pvc\interfaces\struct\collection\factory\CollectionFactoryInterface;
 use pvc\interfaces\struct\payload\HasPayloadInterface;
 use pvc\interfaces\struct\tree\node\TreenodeAbstractInterface;
 use pvc\interfaces\struct\tree\node_value_object\TreenodeValueObjectInterface;
@@ -35,4 +36,10 @@ interface TreenodeFactoryInterface
      * @return TreenodeAbstractInterface<PayloadType, NodeType, TreeType, CollectionType, ValueObjectType>
      */
     public function makeNode(): TreenodeAbstractInterface;
+
+    /**
+     * getCollectionFactory
+     * @return CollectionFactoryInterface<CollectionType>
+     */
+    public function getCollectionFactory(): CollectionFactoryInterface;
 }
