dedbc64af8a27b68b6d2bee97538d014cc14b38a
Doug Wilbourne <dougwilbourne@gmail.com>
2024-03-27 17:49:59 -0400

    added interfaces for TreenodeOrderedFactoryInterface.php and TreenodeUnorderedFactoryInterface.php

---
+++ src/struct/tree/node/factory/TreenodeOrderedFactoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace pvc\interfaces\struct\tree\node\factory;
+
+use pvc\interfaces\struct\collection\CollectionOrderedInterface;
+use pvc\interfaces\struct\payload\HasPayloadInterface;
+use pvc\interfaces\struct\tree\node\TreenodeOrderedInterface;
+use pvc\interfaces\struct\tree\node_value_object\TreenodeValueObjectOrderedInterface;
+use pvc\interfaces\struct\tree\tree\TreeOrderedInterface;
+
+/**
+ * Class TreenodeOrderedFactoryInterface
+ * @template PayloadType of HasPayloadInterface
+ * @phpcs:ignore
+ * @extends TreenodeFactoryInterface<PayloadType, TreenodeOrderedInterface, TreeOrderedInterface, CollectionOrderedInterface, TreenodeValueObjectOrderedInterface>
+ */
+interface TreenodeOrderedFactoryInterface extends TreenodeFactoryInterface
+{
+    /**
+     * makeNode
+     * @return TreenodeOrderedInterface<PayloadType>
+     */
+    public function makeNode(): TreenodeOrderedInterface;
+}
