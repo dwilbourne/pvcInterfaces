04834d5597eda7d7229227b03d482cc4630f1dc5
Doug Wilbourne <dougwilbourne@gmail.com>
2024-03-29 10:56:33 -0400

    reverted to having treenode factories extend treenodeabstractfactory

--- src/struct/tree/node/factory/TreenodeUnorderedFactoryInterface.php
+++ src/struct/tree/node/factory/TreenodeUnorderedFactoryInterface.php
@@ -8,18 +8,24 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\node\factory;
 
+use pvc\interfaces\struct\collection\CollectionUnorderedInterface;
 use pvc\interfaces\struct\payload\HasPayloadInterface;
+use pvc\interfaces\struct\tree\node\TreenodeAbstractInterface;
 use pvc\interfaces\struct\tree\node\TreenodeUnorderedInterface;
+use pvc\interfaces\struct\tree\node_value_object\TreenodeValueObjectUnorderedInterface;
+use pvc\interfaces\struct\tree\tree\TreeUnorderedInterface;
 
 /**
  * Class TreenodeUnorderedFactoryInterface
  * @template PayloadType of HasPayloadInterface
+ * @phpcs:ignore
+ * @extends TreenodeFactoryInterface<PayloadType, TreenodeUnorderedInterface, TreeUnorderedInterface, CollectionUnorderedInterface, TreenodeValueObjectUnorderedInterface>
  */
-interface TreenodeUnorderedFactoryInterface
+interface TreenodeUnorderedFactoryInterface extends TreenodeFactoryInterface
 {
     /**
      * makeNode
      * @return TreenodeUnorderedInterface<PayloadType>
      */
-    public function makeNode(): TreenodeUnorderedInterface;
+    public function makeNode(): TreenodeAbstractInterface;
 }
