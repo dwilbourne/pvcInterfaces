ef16f2f7871fbe5ace49ebac1d2a665a9d6c046f
Doug Wilbourne <dougwilbourne@gmail.com>
2024-04-22 15:49:33 -0400

    added setters and getters for MsgFrmtr and Locale inside FrmtrHtmlInterface.php

--- src/frmtr/html/FrmtrHtmlInterface.php
+++ src/frmtr/html/FrmtrHtmlInterface.php
@@ -9,8 +9,10 @@ declare(strict_types=1);
 namespace pvc\interfaces\frmtr\html;
 
 use pvc\interfaces\frmtr\FrmtrInterface;
+use pvc\interfaces\frmtr\msg\FrmtrMsgInterface;
 use pvc\interfaces\html\tag\TagInterface;
 use pvc\interfaces\html\tag\TagVoidInterface;
+use pvc\interfaces\intl\LocaleInterface;
 
 /**
  * Class FrmtrHtmlInterface
@@ -24,4 +26,28 @@ interface FrmtrHtmlInterface extends FrmtrInterface
      * @return string
      */
     public function format($value): string;
+
+    /**
+     * setMsgFrmtr
+     * @param FrmtrMsgInterface $frmtrMsg
+     */
+    public function setMsgFrmtr(FrmtrMsgInterface $frmtrMsg): void;
+
+    /**
+     * getMsgFrmtr
+     * @return FrmtrMsgInterface
+     */
+    public function getMsgFrmtr(): FrmtrMsgInterface;
+
+    /**
+     * setLocale
+     * @param LocaleInterface $locale
+     */
+    public function setLocale(LocaleInterface $locale): void;
+
+    /**
+     * getLocale
+     * @return LocaleInterface
+     */
+    public function getLocale(): LocaleInterface;
 }
