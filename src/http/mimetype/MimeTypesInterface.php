054459f6f5aed6652169463c08c75fde927e9cb6
Doug Wilbourne <dougwilbourne@gmail.com>
2024-08-05 09:26:46 -0400

    updated MimeTypesInterface.php

--- src/http/mimetype/MimeTypesInterface.php
+++ src/http/mimetype/MimeTypesInterface.php
@@ -16,14 +16,21 @@ interface MimeTypesInterface
     /**
      * getMimeType
      * @param string $fileExt
-     * @return string
+     * @return ?string
      */
-    public function getMimeType(string $fileExt): string;
+    public function getMimeTypeFromExtension(string $fileExt): ?string;
 
     /**
      * getExtensions
      * @param string $mimeType
      * @return array<string>
      */
-    public function getExtensions(string $mimeType): array;
+    public function getExtensionsFromMimeType(string $mimeType): array;
+
+    /**
+     * isValidMimeType
+     * @param string $mimeType
+     * @return bool
+     */
+    public function isValidMimeType(string $mimeType): bool;
 }
