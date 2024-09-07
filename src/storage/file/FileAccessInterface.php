53ea377e2b24e16ea92d235b76cc5b4cf4fa1973
Doug Wilbourne <dougwilbourne@gmail.com>
2024-09-07 11:50:51 -0400

    Added FileAccessInterface

---
+++ src/storage/file/FileAccessInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+declare(strict_types=1);
+
+namespace pvc\interfaces\storage\file;
+
+/**
+ * Class FileAccessInterface
+ */
+interface FileAccessInterface
+{
+    public function fileExists(string $filename): bool;
+
+    public function fileIsReadable(string $filename): bool;
+
+    public function fileIsWriteable(string $filename): bool;
+
+    public function fileGetContents(string $filename): string|false;
+
+    public function filePutContents(string $filename, string $data): bool;
+
+    public function fileGetLine(): string|false;
+
+    public function filePutLine(string $data): bool;
+
+    public function openFile(string $filename, string $mode): bool;
+
+    public function readFile(int $length = 8096);
+
+    public function writeFile(string $data): bool;
+
+    public function closeFile(): bool;
+
+    public function eof(): bool;
+
+    public function directoryExists(string $dirname): bool;
+
+    public function directoryIsWriteable(string $dirname): bool;
+
+    public function directoryIsReadable(string $dirname): bool;
+
+    public function getDirectoryContents(string $dirname, bool $withDots = false): ?array;
+}
