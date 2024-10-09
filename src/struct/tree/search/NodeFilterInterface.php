90663c34cd217cedab49102784839fcfe18bd82f
Doug Wilbourne <dougwilbourne@gmail.com>
2024-10-09 14:15:04 -0400

    removed FileFilterInterface and made NodeFilterInterface generic so it will work with more than just Trees from the pvc struct library.  For example, it can be used for searching a file system.

--- src/struct/tree/search/NodeFilterInterface.php
+++ src/struct/tree/search/NodeFilterInterface.php
@@ -8,26 +8,16 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\search;
 
-use pvc\interfaces\struct\collection\CollectionAbstractInterface;
-use pvc\interfaces\struct\payload\HasPayloadInterface;
-use pvc\interfaces\struct\tree\node\TreenodeAbstractInterface;
-use pvc\interfaces\struct\tree\node_value_object\TreenodeValueObjectInterface;
-use pvc\interfaces\struct\tree\tree\TreeAbstractInterface;
-
 /**
  * Class NodeFilterInterface
- * @template PayloadType of HasPayloadInterface
- * @template NodeType of TreenodeAbstractInterface
- * @template TreeType of TreeAbstractInterface
- * @template CollectionType of CollectionAbstractInterface
- * @template ValueObjectType of TreenodeValueObjectInterface
+ * @template NodeType
  */
 interface NodeFilterInterface
 {
     /**
      * testNode
-     * @param TreenodeAbstractInterface<PayloadType, NodeType, TreeType, CollectionType, ValueObjectType> $node
+     * @param NodeType $node
      * @return bool
      */
-    public function testNode(TreenodeAbstractInterface $node): bool;
+    public function testNode($node): bool;
 }
