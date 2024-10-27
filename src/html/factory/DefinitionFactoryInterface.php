0937f58b616dd5dce5c44d9191c329addf9b9429
Doug Wilbourne <dougwilbourne@gmail.com>
2024-10-27 13:50:52 -0400

    updated DefinitionFactoryInterface.php to include parameter list for each method.

--- src/html/factory/DefinitionFactoryInterface.php
+++ src/html/factory/DefinitionFactoryInterface.php
@@ -15,19 +15,51 @@ interface DefinitionFactoryInterface
 {
     /**
      * makeElementDefinition
+     * @param string $elementName
+     * @param string $tagType
+     * @param array<string> $allowedAttributes
+     * @param array<string> $allowedSubTags
      * @return string
      */
-    public function makeElementDefinition(): string;
+    public function makeElementDefinition(
+        string $elementName,
+        string $tagType,
+        array $allowedAttributes,
+        array $allowedSubTags
+    ): string;
 
     /**
      * makeAttributeDefinition
+     * @param string $attributeName
+     * @param string $attributeType
+     * @param string $valTesterName
+     * @param string $global
      * @return string
      */
-    public function makeAttributeDefinition(): string;
+    public function makeAttributeDefinition(
+        string $attributeName,
+        string $attributeType,
+        string $valTesterName,
+        string $global
+    ): string;
+
+    /**
+     * makeAttributeValueTesterDefinition
+     * @param string $testerName
+     * @param string $testerType
+     * @param string $testerArg
+     * @return string
+     */
+    public function makeAttributeValueTesterDefinition(
+        string $testerName,
+        string $testerType,
+        string $testerArg
+    ): string;
 
     /**
      * makeEventDefinition
+     * @param string $eventName
      * @return string
      */
-    public function makeEventDefinition(): string;
+    public function makeEventDefinition(string $eventName): string;
 }
