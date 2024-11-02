d5e2b4831ccbd73e8b9b8c3889ce11d3ba82ce79
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-02 13:05:35 -0400

    added setName and setTester methods to AttributeCustomDataInterface.php

--- src/html/attribute/AttributeCustomDataInterface.php
+++ src/html/attribute/AttributeCustomDataInterface.php
@@ -8,9 +8,25 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\html\attribute;
 
+use pvc\interfaces\validator\ValTesterInterface;
+
 /**
  * Class AttributeCustomDataInterface
+ *
+ * custom attributes are essentially single valued attributes, but we need methods to allow the programmer to set
+ * the name / id and the value tester manually as opposed to through the DI container.
  */
 interface AttributeCustomDataInterface extends AttributeSingleValueInterface
 {
+    /**
+     * setName
+     * @param string $name
+     */
+    public function setName(string $name): void;
+
+    /**
+     * setTester
+     * @param ValTesterInterface<string> $valTester
+     */
+    public function setTester(ValTesterInterface $valTester): void;
 }
