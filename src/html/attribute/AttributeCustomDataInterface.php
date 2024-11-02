ef5ae7d520c522aec7454f1e1604836a96986b91
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-02 12:36:04 -0400

    added AttributeCustomDataInterface.php

---
+++ src/html/attribute/AttributeCustomDataInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace pvc\interfaces\html\attribute;
+
+/**
+ * Class AttributeCustomDataInterface
+ */
+interface AttributeCustomDataInterface extends AttributeSingleValueInterface
+{
+}
