67a6abbc7a0ad0be7814483dffe4052ffff8478a
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-09 16:41:19 -0500

    modified array shape types for the various definitions to differentiate more eaily between the concrete class being created and the type of definition

--- src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
+++ src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
@@ -15,17 +15,19 @@ namespace pvc\interfaces\html\factory\definitions;
  * DefinitionType is an enum of the kinds of definitions this library supports, @see DefinitionType.php
  *
  * @phpcs:ignore
- * @phpstan-type ElementDef array{'DefId':string,'Name':string,'Comment':string,'Type':string,
+ * @phpstan-type ElementDef array{'DefId':string,'DefType':string,'Name':string,'Comment':string,'Concrete':string,
  *     'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
  *
  * @phpcs:ignore
- * @phpstan-type AttributeDef array{'DefId':string,'Name':string,'Type':string,'ValueTester':string,'Global':bool,'CaseSensitive':bool}
+ * @phpstan-type AttributeDef array{'DefId':string,'DefType':string,'Name':string,'Concrete':string,
+ *     'ValueTester':string,'CaseSensitive':bool,'Global':bool}
  *
  * @phpcs:ignore
- * @phpstan-type AttributeValueTesterDef array{'DefId':string,'Name':string,'Type':string,'TesterArg':string|array<string>}
+ * @phpstan-type AttributeValueTesterDef array{'DefId':string,'DefType':string,'Name':string,'Concrete':string,
+ * 'TesterArg':string|array<string>}
  *
  * @phpcs:ignore
- * @phpstan-type EventDef array{'DefId':string, 'Name':string, 'Type':string}
+ * @phpstan-type EventDef array{'DefId':string,'DefType':string,'Name':string, 'Concrete':string}
  *
  * @phpcs:ignore
  * @phpstan-type DefinitionFileNames array{'Attribute':string,'AttributeValueTester':string,'Element':string,'Event':string}
