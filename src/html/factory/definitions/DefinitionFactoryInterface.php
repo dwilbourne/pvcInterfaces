8aa8f523bdeccc20d5eb898c39bbdba53eb69252
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-11 11:17:02 -0500

    simplified DefinitionfactoryInterface down to a single method called makeDefinition

--- src/html/factory/definitions/DefinitionFactoryInterface.php
+++ src/html/factory/definitions/DefinitionFactoryInterface.php
@@ -12,21 +12,17 @@ namespace pvc\interfaces\html\factory\definitions;
  * Class DefinitionFactoryInterface
  * @template Definition
  *
- * @phpcs:ignore
  * @phpstan-type ElementDef array{'DefId':string,'DefType':string,'Name':string,'Comment':string,'Concrete':string,
  *      'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
  *
- * @phpcs:ignore
  * @phpstan-type AttributeDef array{'DefId':string,'DefType':string,'Name':string,'Concrete':string,
  *     'ValueTester':string,'CaseSensitive':bool,'Global':bool}
  *
- * @phpcs:ignore
- * @phpstan-type AttributeValueTesterDef array{'DefId':string,'DefType':string,'Concrete':string,'Arg':string|array<string>}
+ * @phpstan-type AttributeValueTesterDef array{'DefId':string,'DefType':string,'Concrete':string,
+ *     'Arg':string|array<string>}
  *
- * @phpcs:ignore
  * @phpstan-type EventDef array{'DefId':string,'DefType':string,'Concrete':string}
  *
- * @phpcs:ignore
  * @phpstan-type OtherDef array{'DefId':string,'DefType':string,'Concrete':string,'Arg':string,'Shared':bool}
  *
  * Only the value tester definitions should be 'shared'.  E.g. When you request an attribute, element or event from the
@@ -35,37 +31,9 @@ namespace pvc\interfaces\html\factory\definitions;
 interface DefinitionFactoryInterface
 {
     /**
-     * makeAttributeDefinition
-     * @param AttributeDef $attributeDef
+     * makeDefinition
+     * @param array<mixed> $defArray
      * @return Definition
      */
-    public function makeAttributeDefinition(array $attributeDef): mixed;
-
-    /**
-     * makeAttributeValueTesterDefinition
-     * @param AttributeValueTesterDef $attributeValueTesterDef
-     * @return mixed
-     */
-    public function makeAttributeValueTesterDefinition(array $attributeValueTesterDef): mixed;
-
-    /**
-     * makeElementDefinition
-     * @param ElementDef $elementDef
-     * @return Definition
-     */
-    public function makeElementDefinition(array $elementDef): mixed;
-
-    /**
-     * makeEventDefinition
-     * @param EventDef $eventDef
-     * @return Definition
-     */
-    public function makeEventDefinition(array $eventDef): mixed;
-
-    /**
-     * makeOtherSharedDefinition
-     * @param OtherDef $otherDef
-     * @return mixed
-     */
-    public function makeOtherDefinition(array $otherDef): mixed;
+    public function makeDefinition(array $defArray): mixed;
 }
