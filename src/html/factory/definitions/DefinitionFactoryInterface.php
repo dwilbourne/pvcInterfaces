400b1a79f660adb6cbb7c65c955f1e181a08debb
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-11 12:45:17 -0500

    updated parameter type in makeDefinition

--- src/html/factory/definitions/DefinitionFactoryInterface.php
+++ src/html/factory/definitions/DefinitionFactoryInterface.php
@@ -12,6 +12,8 @@ namespace pvc\interfaces\html\factory\definitions;
  * Class DefinitionFactoryInterface
  * @template Definition
  *
+ * creates a definition which is native to the container from a definition object (which was created from the json)
+ *
  * Only the value tester definitions should be 'shared'.  E.g. When you request an attribute, element or event from the
  * container, it should produce a new instance of the object. Value Tester objects are stateless and can be shared.
  */
@@ -19,8 +21,8 @@ interface DefinitionFactoryInterface
 {
     /**
      * makeDefinition
-     * @param array<mixed> $defArray
+     * @param DefinitionInterface $def
      * @return Definition
      */
-    public function makeDefinition(array $defArray): mixed;
+    public function makeDefinition(DefinitionInterface $def): mixed;
 }
