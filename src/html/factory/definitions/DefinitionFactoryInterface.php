a14a020790416d99a3f4bbd37f55e1bdf674af59
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-11 11:22:23 -0500

    updated data type of DefType attribute of the definition arrays to be DefinitionType

--- src/html/factory/definitions/DefinitionFactoryInterface.php
+++ src/html/factory/definitions/DefinitionFactoryInterface.php
@@ -12,18 +12,19 @@ namespace pvc\interfaces\html\factory\definitions;
  * Class DefinitionFactoryInterface
  * @template Definition
  *
- * @phpstan-type ElementDef array{'DefId':string,'DefType':string,'Name':string,'Comment':string,'Concrete':string,
+ * @phpstan-type ElementDef array{'DefId':string,'DefType':DefinitionType,'Name':string,'Comment':string,
+ *     'Concrete':string,
  *      'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
  *
- * @phpstan-type AttributeDef array{'DefId':string,'DefType':string,'Name':string,'Concrete':string,
+ * @phpstan-type AttributeDef array{'DefId':string,'DefType':DefinitionType,'Name':string,'Concrete':string,
  *     'ValueTester':string,'CaseSensitive':bool,'Global':bool}
  *
- * @phpstan-type AttributeValueTesterDef array{'DefId':string,'DefType':string,'Concrete':string,
+ * @phpstan-type AttributeValueTesterDef array{'DefId':string,'DefType':DefinitionType,'Concrete':string,
  *     'Arg':string|array<string>}
  *
- * @phpstan-type EventDef array{'DefId':string,'DefType':string,'Concrete':string}
+ * @phpstan-type EventDef array{'DefId':string,'DefType':DefinitionType,'Concrete':string}
  *
- * @phpstan-type OtherDef array{'DefId':string,'DefType':string,'Concrete':string,'Arg':string,'Shared':bool}
+ * @phpstan-type OtherDef array{'DefId':string,'DefType':DefinitionType,'Concrete':string,'Arg':string,'Shared':bool}
  *
  * Only the value tester definitions should be 'shared'.  E.g. When you request an attribute, element or event from the
  * container, it should produce a new instance of the object. Value Tester objects are stateless and can be shared.
