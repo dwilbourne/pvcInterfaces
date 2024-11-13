60a66a5f14aac5f1d700a8b4f89bd8d3bbabad18
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-12 19:58:05 -0500

    updated documentation in AbstractDefinitionFactoryInterface.php

--- src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
+++ src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
@@ -11,9 +11,18 @@ namespace pvc\interfaces\html\factory\definitions;
 /**
  * Class AbstractDefinitionFactoryInterface
  *
+ * OK, so this is something of a hack, but the idea is to create a theoretical array shape that is contravariant to
+ * all the definition array shapes.  Doing so gets rid of the liskov problem when we pass an array of this shape
+ * in as a parameter to the specific factory methods defined in DefinitionFactoryInterface.
+ *
+ * If we were using templates to describe the array shapes, we could use @template-contravariant in the declaration of
+ * the generic.  But we need array shapes, not templates, in order to let phpstan now that the various array indices
+ * exist.
+ *
  * @phpstan-type DefArray array{'DefId':string,'DefType':string,'Concrete':class-string,'Comment':string,
  *     'Name':string, 'ValTester':class-string,'CaseSensitive':bool,'Global':bool,'Arg':class-string|null,
  *     'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
+ *
  * @template VendorSpecificDefinition
  */
 interface AbstractDefinitionFactoryInterface
