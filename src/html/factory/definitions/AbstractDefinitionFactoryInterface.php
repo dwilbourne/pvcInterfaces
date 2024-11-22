f388928c9d32e459bdf0e55021c99e450658d6c1
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-22 17:35:21 -0500

    updated index names in the phpstan array shape definition DefArray

--- src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
+++ src/html/factory/definitions/AbstractDefinitionFactoryInterface.php
@@ -15,13 +15,13 @@ namespace pvc\interfaces\html\factory\definitions;
  * all the definition array shapes.  Doing so gets rid of the liskov problem when we pass an array of this shape
  * in as a parameter to the specific factory methods defined in DefinitionFactoryInterface.
  *
- * If we were using templates to describe the array shapes, we could use @template-contravariant in the declaration of
+ * If we could use templates to describe the array shapes, we could use @template-contravariant in the declaration of
  * the generic.  But we need array shapes, not templates, in order to let phpstan now that the various array indices
  * exist.
  *
- * @phpstan-type DefArray array{'DefId':string,'DefType':string,'Concrete':class-string,'Comment':string,
- *     'Name':string, 'ValTester':class-string,'CaseSensitive':bool,'Global':bool,'Arg':class-string|null,
- *     'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
+ * @phpstan-type DefArray array{'defId':string,'defType':string,'concrete':class-string,'comment':string,
+ *     'name':string, 'valTester':class-string,'caseSensitive':bool,'global':bool,'arg':class-string|null,
+ *     'allowedAttributeDefIds':array<string>, 'allowedChildDefIds':array<string>}
  *
  * @template VendorSpecificDefinition
  */
