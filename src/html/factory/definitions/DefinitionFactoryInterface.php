fae1c025e19c498538729ddb8de053578253d6c2
Doug Wilbourne <dougwilbourne@gmail.com>
2024-11-23 08:49:01 -0500

    updated types of array parameters to DefType

--- src/html/factory/definitions/DefinitionFactoryInterface.php
+++ src/html/factory/definitions/DefinitionFactoryInterface.php
@@ -10,20 +10,7 @@ namespace pvc\interfaces\html\factory\definitions;
 
 /**
  * Class DefinitionFactoryInterface
- *
- * @phpstan-type AttributeDefArray array{'DefId':string, 'DefType':string, 'Concrete':class-string, 'Name':string,
- * 'ValTester':class-string, 'CaseSensitive':bool, 'Global':bool}
- *
- * @phpstan-type AttributeValTesterDefArray array{'DefId':string, 'DefType':string, 'Concrete':class-string,
- *     'Arg':class-string|null}
- *
- * @phpstan-type ElementDefArray array{'DefId':string, 'DefType':string, 'Concrete':class-string, 'Name':string,
- *     'AllowedAttributeDefIds':array<string>, 'AllowedChildDefIds':array<string>}
- *
- * @phpstan-type EventDefArray array{'DefId':string, 'DefType':string, 'Concrete':class-string}
- *
- * @phpstan-type OtherDefArray array{'DefId':string, 'DefType':string, 'Concrete':class-string,'Arg':class-string|null}
- *
+ * @phpstan-import-type DefArray from AbstractDefinitionFactoryInterface
  * @template VendorSpecificDefinition
  *
  * creates a definition which is native to the container from a definition array which was created from the
@@ -36,35 +23,35 @@ interface DefinitionFactoryInterface
 {
     /**
      * makeAttributeDefinition
-     * @param AttributeDefArray $definitionArray
+     * @param DefArray $definitionArray
      * @return VendorSpecificDefinition
      */
     public function makeAttributeDefinition(array $definitionArray): mixed;
 
     /**
      * makeAttributeValueTesterDefinition
-     * @param AttributeValTesterDefArray $definitionArray
+     * @param DefArray $definitionArray
      * @return VendorSpecificDefinition
      */
     public function makeAttributeValueTesterDefinition(array $definitionArray): mixed;
 
     /**
      * makeElementDefinition
-     * @param ElementDefArray $definitionArray
+     * @param DefArray $definitionArray
      * @return VendorSpecificDefinition
      */
     public function makeElementDefinition(array $definitionArray): mixed;
 
     /**
      * makeEventDefinition
-     * @param EventDefArray $definitionArray
+     * @param DefArray $definitionArray
      * @return VendorSpecificDefinition
      */
     public function makeEventDefinition(array $definitionArray): mixed;
 
     /**
      * makeOtherDefinition
-     * @param OtherDefArray $definitionArray
+     * @param DefArray $definitionArray
      * @return VendorSpecificDefinition
      */
     public function makeOtherDefinition(array $definitionArray): mixed;
