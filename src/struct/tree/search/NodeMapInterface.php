7b02f7c5b2fff1b6f211922f5caa3593077a9129
Doug Wilbourne <dougwilbourne@gmail.com>
2025-01-04 16:43:45 -0500

    added getNodeMapAsArray method to NodeMapInterface.php

--- src/struct/tree/search/NodeMapInterface.php
+++ src/struct/tree/search/NodeMapInterface.php
@@ -10,7 +10,7 @@ namespace pvc\interfaces\struct\tree\search;
 
 /**
  * Class NodeMapInterface
- * helper object for DepthFirst Searches
+ * @template NodeType of NodeSearchableInterface
  */
 interface NodeMapInterface
 {
@@ -29,7 +29,7 @@ interface NodeMapInterface
      * setNode
      * @param non-negative-int $nodeId
      * @param non-negative-int|null $parentId
-     * @param NodeSearchableInterface $node
+     * @param NodeType $node
      */
     public function setNode(int $nodeId, ?int $parentId, NodeSearchableInterface $node): void;
 
@@ -43,14 +43,20 @@ interface NodeMapInterface
     /**
      * getParent
      * @param int $nodeId
-     * @return NodeSearchableInterface|null
+     * @return NodeType|null
      */
     public function getParent(int $nodeId): ?NodeSearchableInterface;
 
     /**
      * getNode
      * @param int $nodeId
-     * @return NodeSearchableInterface|null
+     * @return NodeType|null
      */
     public function getNode(int $nodeId): ?NodeSearchableInterface;
+
+    /**
+     * getNodeMapAsArray
+     * @return array<NodeType>
+     */
+    public function getNodeMapAsArray(): array;
 }
