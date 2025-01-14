d7143ff4f15108faf3b556f24c4e9bd321ae2fd3
Doug Wilbourne <dougwilbourne@gmail.com>
2025-01-14 17:37:02 -0500

    updated array shape of node data in TreenodeDTOInterface.php

--- src/struct/tree/dto/TreenodeDTOInterface.php
+++ src/struct/tree/dto/TreenodeDTOInterface.php
@@ -20,6 +20,8 @@ use pvc\interfaces\struct\tree\tree\TreeAbstractInterface;
  * @template TreeType of TreeAbstractInterface
  * @template CollectionType of CollectionAbstractInterface
  * @template DtoType of TreenodeDTOInterface
+ * @phpcs:ignore
+ * @phpstan-type NodeData array{'nodeId': non-negative-int,'parentId': non-negative-int|null, 'treeId':non-negative-int,'payload': PayloadType, 'index'?:non-negative-int}
  */
 interface TreenodeDTOInterface
 {
@@ -31,7 +33,7 @@ interface TreenodeDTOInterface
 
     /**
      * hydrateFromArray
-     * @param array<mixed> $values
+     * @param NodeData $values
      */
     public function hydrateFromArray(array $values): void;
 }
