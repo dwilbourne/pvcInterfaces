bf57a2976d6cc17f113bc3fec8d41c3f0d6a5bc6
Doug Wilbourne <dougwilbourne@gmail.com>
2025-01-19 14:29:14 -0500

    updated HasPayloadInterface::getPayloadTester so it cannot return null and always returns valTesterInterface

--- src/struct/payload/HasPayloadInterface.php
+++ src/struct/payload/HasPayloadInterface.php
@@ -12,8 +12,13 @@ use pvc\interfaces\validator\ValTesterInterface;
 
 /**
  * Class HasPayloadInterface
- * All data structures implement HasPayloadInterface so that the methods for accessing the data within
- * the structures remain consistent across all types
+ *
+ * All data structures in this namespapce implement HasPayloadInterface so that the methods for accessing the data
+ * within the structures remain consistent across all types.
+ *
+ * The data typing in the interface is set up so that payloads can be null in implementation if you want them to be
+ * nullable.  Of course, you can prevent them from being nullable as well by setting up the payloadtester accordingly.
+ *
  * @template PayloadType
  */
 interface HasPayloadInterface
@@ -40,5 +45,5 @@ interface HasPayloadInterface
      * getPayloadTester
      * @return ValTesterInterface<PayloadType>|null
      */
-    public function getPayloadTester(): ?ValTesterInterface;
+    public function getPayloadTester(): ValTesterInterface;
 }
