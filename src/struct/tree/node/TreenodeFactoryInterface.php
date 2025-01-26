28ac8cf5ac2aae76c2384fa9823fdb94520a0301
Doug Wilbourne <dougwilbourne@gmail.com>
2025-01-26 13:28:34 -0500

    added getCollectionFactory method to TreenodeFactoryInterface.php

--- src/struct/tree/node/TreenodeFactoryInterface.php
+++ src/struct/tree/node/TreenodeFactoryInterface.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\node;
 
+use pvc\interfaces\struct\collection\CollectionFactoryInterface;
 use pvc\interfaces\struct\payload\HasPayloadInterface;
 
 /**
@@ -21,4 +22,12 @@ interface TreenodeFactoryInterface
      * @return TreenodeInterface<PayloadType>
      */
     public function makeNode(): TreenodeInterface;
+
+    /**
+     * @return CollectionFactoryInterface<PayloadType>
+     * this is in here for an edge case where someone is trying to get the siblings of the root node of the tree.
+     * Because root has no siblings (e.g. there is no parent and thus there is no collection of siblings) we need
+     * to be abel to make an empty Collection to return when someone asks for the siblings of the root.
+     */
+    public function getCollectionFactory(): CollectionFactoryInterface;
 }
