b286a5d9907d48c7de882bf5e6d97c48ae057f93
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-06 10:08:57 -0500

    added makeDto to DtoFactoryInterface.php

---
+++ src/struct/dto/DtoFactoryAbstractInterface.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace pvc\interfaces\struct\dto;
+
+use pvc\interfaces\struct\payload\HasPayloadInterface;
+
+/**
+ * @template PayloadType of HasPayloadInterface
+ * @template EntityType
+ */
+interface DtoFactoryAbstractInterface
+{
+    /**
+     * @param string $dtoPropertyName
+     * @param string $entityGetterMethodName
+     * @param string $arrayKeyName
+     * @return void
+     */
+    public function setPropertyMap(
+        string $dtoPropertyName,
+        string $entityGetterMethodName,
+        string $arrayKeyName,
+    ): void;
+
+    /**
+     * @param array<mixed> $values
+     * @return DtoInterface<PayloadType>
+     */
+    public function hydrateDtoFromArray(array $values): DtoInterface;
+
+    /**
+     * @param EntityType $entity
+     * @return DtoInterface<PayloadType>
+     */
+    public function hydrateDtoFromEntity(mixed $entity): DtoInterface;
+
+    /**
+     * @param mixed $entity
+     * @return array<mixed>
+     */
+    public function makeArrayFromEntity(mixed $entity): array;
+}
--- src/struct/dto/DtoFactoryInterface.php
+++ src/struct/dto/DtoFactoryInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace pvc\interfaces\struct\dto;
@@ -7,42 +8,11 @@ use pvc\interfaces\struct\payload\HasPayloadInterface;
 
 /**
  * @template PayloadType of HasPayloadInterface
- * @template EntityType
  */
 interface DtoFactoryInterface
 {
     /**
-     * @param string $dtoPropertyName
-     * @param string $entityGetterMethodName
-     * @param string $arrayKeyName
-     * @return void
-     */
-    public function setPropertyMap(
-        string $dtoPropertyName,
-        string $entityGetterMethodName,
-        string $arrayKeyName,
-    ): void;
-
-    /**
      * @return DtoInterface<PayloadType>
      */
     public function makeDto(): DtoInterface;
-
-    /**
-     * @param array<mixed> $values
-     * @return DtoInterface<PayloadType>
-     */
-    public function hydrateDtoFromArray(array $values): DtoInterface;
-
-    /**
-     * @param EntityType $entity
-     * @return DtoInterface<PayloadType>
-     */
-    public function hydrateDtoFromEntity(mixed $entity): DtoInterface;
-
-    /**
-     * @param mixed $entity
-     * @return array<mixed>
-     */
-    public function makeArrayFromEntity(mixed $entity): array;
 }
