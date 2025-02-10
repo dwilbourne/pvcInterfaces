e8974886d5ebc4d8388cf19db77ef631e5109fcf
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-10 10:07:04 -0500

    added TreenodeCollectionInterface.php

---
+++ src/struct/tree/node/TreenodeCollectionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pvc\interfaces\struct\tree\node;
+
+use pvc\interfaces\struct\payload\HasPayloadInterface;
+
+/**
+ * @template PayloadType of HasPayloadInterface
+ */
+interface TreenodeCollectionInterface
+{
+
+}
