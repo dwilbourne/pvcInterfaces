a944e90eed8fd48e6d055744d078974cac03853c
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-10 10:39:48 -0500

    updated TreenodeCollectionInterface.php to extend Iterator

--- src/struct/tree/collection/TreenodeCollectionInterface.php
+++ src/struct/tree/collection/TreenodeCollectionInterface.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\collection;
 
+use Iterator;
 use pvc\interfaces\struct\payload\HasPayloadInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 /**
  * @template PayloadType of HasPayloadInterface
+ * @extends Iterator<TreenodeInterface<PayloadType>>
  */
-interface TreenodeCollectionInterface
+interface TreenodeCollectionInterface extends Iterator
 {
     /**
      * @return bool
