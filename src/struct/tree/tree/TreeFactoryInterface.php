837e546e1f2793655cf583db2510393fdd3779ca
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-17 11:20:55 -0500

    added parameters to makeTree method of TreeFactoryInterface.php so that it can make dependent objects as well as the tree all at once

--- src/struct/tree/tree/TreeFactoryInterface.php
+++ src/struct/tree/tree/TreeFactoryInterface.php
@@ -8,9 +8,10 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\tree;
 
+use pvc\interfaces\struct\collection\CollectionFactoryInterface;
 use pvc\interfaces\struct\tree\dto\TreenodeDtoInterface;
 use pvc\interfaces\struct\tree\dto\TreenodeDtoSorterInterface;
-use pvc\interfaces\struct\tree\node\TreenodeFactoryInterface;
+use pvc\interfaces\validator\ValTesterInterface;
 
 /**
  * Class TreeFactoryInterface
@@ -20,16 +21,18 @@ interface TreeFactoryInterface
 {
     /**
      * makeTree
-     * @param TreenodeFactoryInterface<PayloadType> $treenodeFactory
+     * @param CollectionFactoryInterface<PayloadType> $collectionFactory
+     * @param ValTesterInterface<PayloadType> $payloadTester,
      * @param TreenodeDtoSorterInterface<PayloadType> $sorter
      * @param int $treeId
      * @param array<TreenodeDtoInterface<PayloadType>> $dtoArray
      * @return TreeInterface<PayloadType>
      */
     public static function makeTree(
-        TreenodeFactoryInterface $treenodeFactory,
+        CollectionFactoryInterface $collectionFactory,
+        ValTesterInterface $payloadTester,
         TreenodeDtoSorterInterface $sorter,
-        int $treeId,
-        array $dtoArray,
+        int                        $treeId,
+        array                      $dtoArray,
     ): TreeInterface;
 }
