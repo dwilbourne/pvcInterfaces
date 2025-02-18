013aba76a5b076e8c440f20deaa92aadcca98794
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-18 11:37:05 -0500

    removed TreenodeDtoSorter as a parameter to TreeFactoryInterface::makeTree

--- src/struct/tree/tree/TreeFactoryInterface.php
+++ src/struct/tree/tree/TreeFactoryInterface.php
@@ -10,7 +10,6 @@ namespace pvc\interfaces\struct\tree\tree;
 
 use pvc\interfaces\struct\collection\CollectionFactoryInterface;
 use pvc\interfaces\struct\tree\dto\TreenodeDtoInterface;
-use pvc\interfaces\struct\tree\dto\TreenodeDtoSorterInterface;
 use pvc\interfaces\validator\ValTesterInterface;
 
 /**
@@ -23,7 +22,6 @@ interface TreeFactoryInterface
      * makeTree
      * @param CollectionFactoryInterface<PayloadType> $collectionFactory
      * @param ValTesterInterface<PayloadType> $payloadTester,
-     * @param TreenodeDtoSorterInterface<PayloadType> $sorter
      * @param int $treeId
      * @param array<TreenodeDtoInterface<PayloadType>> $dtoArray
      * @return TreeInterface<PayloadType>
@@ -31,7 +29,6 @@ interface TreeFactoryInterface
     public static function makeTree(
         CollectionFactoryInterface $collectionFactory,
         ValTesterInterface $payloadTester,
-        TreenodeDtoSorterInterface $sorter,
         int                        $treeId,
         array                      $dtoArray,
     ): TreeInterface;
