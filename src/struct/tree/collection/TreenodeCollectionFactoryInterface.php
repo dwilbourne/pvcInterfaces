2a94a7b29a7ba25f0460ee81ff5c17c7256c6112
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-20 11:36:35 -0500

    added array parameter to TreenodeCollectionFactoryInterface::makeCollection

--- src/struct/tree/collection/TreenodeCollectionFactoryInterface.php
+++ src/struct/tree/collection/TreenodeCollectionFactoryInterface.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\collection;
 
+use pvc\interfaces\struct\tree\node\TreenodeInterface;
+
 /**
  * @template PayloadType
  */
 interface TreenodeCollectionFactoryInterface
 {
     /**
+     * @param array<TreenodeInterface<PayloadType>> $treenodes
      * @return TreenodeCollectionInterface<PayloadType>
      */
-    public function makeTreenodeCollection(): TreenodeCollectionInterface;
+    public function makeTreenodeCollection(array $treenodes): TreenodeCollectionInterface;
 }
