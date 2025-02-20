8cc64ec8bb6e0c8d2fd32e4cee099ff5b79f8cb5
Doug Wilbourne <dougwilbourne@gmail.com>
2025-02-20 16:23:46 -0500

    updated TreenodeDtoCollectionInterface.php to extend Countable

--- src/struct/tree/dto/TreenodeDtoCollectionInterface.php
+++ src/struct/tree/dto/TreenodeDtoCollectionInterface.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\dto;
 
+use Countable;
 use Iterator;
 
 /**
  * @template PayloadType
  * @extends Iterator<TreenodeDtoInterface<PayloadType>>
  */
-interface TreenodeDtoCollectionInterface extends Iterator
+interface TreenodeDtoCollectionInterface extends Iterator, Countable
 {
 }
