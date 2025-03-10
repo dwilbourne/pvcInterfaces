342c96a4f645e213cf89dd2bd7b7a8917e7067b9
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-10 10:45:19 -0400

    updated shape of array NodeMapRow in NodeMapInterface.php

--- src/struct/tree/search/NodeMapInterface.php
+++ src/struct/tree/search/NodeMapInterface.php
@@ -21,7 +21,7 @@ namespace pvc\interfaces\struct\tree\search;
  * whatever is being searched so that is possible to move up and down the tree with integer pointers.  In turn,
  * this facilitates inserting the nodes into a pvc tree structure for further manipulation.
  *
- * @phpstan-type NodeMapRow array{parentId:non-negative-int|null, node:NodeVisitableInterface}
+ * @phpstan-type NodeMapRow array{nodeid: non-negative-int, parentId:non-negative-int|null, node:NodeVisitableInterface}
  */
 interface NodeMapInterface
 {
