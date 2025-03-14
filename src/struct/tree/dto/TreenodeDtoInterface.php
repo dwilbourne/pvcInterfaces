fbc4dd02e42f14f150a6fa40a775ce192e358c8e
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-14 16:27:02 -0400

    reverted TreenodeDtoInterface.php so it extends CollectionElementInterface

--- src/struct/tree/dto/TreenodeDtoInterface.php
+++ src/struct/tree/dto/TreenodeDtoInterface.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\dto;
 
-use pvc\interfaces\struct\collection\CollectionIndexedElementInterface;
 use pvc\interfaces\struct\dto\DtoInterface;
 
 /**
  * @template PayloadType
  * @phpstan-type TreenodeDtoShape object{'nodeId': non-negative-int, 'parentId': ?non-negative-int, 'treeId': ?non-negative-int, 'payload': mixed, 'index': non-negative-int}
  */
-interface TreenodeDtoInterface extends DtoInterface, CollectionIndexedElementInterface
+interface TreenodeDtoInterface extends DtoInterface
 {
 }
