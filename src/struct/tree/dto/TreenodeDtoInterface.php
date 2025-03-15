abe687f80f22599c5454b6b7a1bd60ed9b2f5901
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-15 10:58:41 -0400

    reverted TreenodeDtoInterface.php

--- src/struct/tree/dto/TreenodeDtoInterface.php
+++ src/struct/tree/dto/TreenodeDtoInterface.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\dto;
 
-use pvc\interfaces\struct\collection\IndexedElementInterface;
 use pvc\interfaces\struct\dto\DtoInterface;
 
 /**
  * @template PayloadType
  * @phpstan-type TreenodeDtoShape object{'nodeId': non-negative-int, 'parentId': ?non-negative-int, 'treeId': ?non-negative-int, 'payload': mixed, 'index': non-negative-int}
  */
-interface TreenodeDtoInterface extends DtoInterface, IndexedElementInterface
+interface TreenodeDtoInterface extends DtoInterface
 {
 }
