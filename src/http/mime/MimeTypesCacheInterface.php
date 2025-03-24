44a55cf1e1f7a2edf29afe35b52eb5eb7d47e6f8
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-24 13:38:01 -0400

    added MimeTypesCacheInterface

---
+++ src/http/mime/MimeTypesCacheInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace pvc\interfaces\http\mime;
+
+use DateInterval;
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * use phpdocs to narrow the types via a static type checker
+ */
+interface MimeTypesCacheInterface extends CacheInterface
+{
+    /**
+     * @param string $key
+     * @param mixed|null $default
+     * @return MimeTypeInterface
+     */
+    public function get(string $key, mixed $default = null): mixed;
+
+    /**
+     * @param string $key
+     * @param MimeTypeInterface $value
+     * @param int|DateInterval|null $ttl
+     * @return bool
+     */
+    public function set(string $key, mixed $value, null|int|DateInterval $ttl = null): bool;
+}
