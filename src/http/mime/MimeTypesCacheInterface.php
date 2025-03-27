14f15ac391e833d3409055d9c9dca7bbe52a3e73
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-26 21:24:41 -0400

    further narrowed MimeTypesCacheInterface.php by removing unnecessary parameters

--- src/http/mime/MimeTypesCacheInterface.php
+++ src/http/mime/MimeTypesCacheInterface.php
@@ -4,24 +4,17 @@ namespace pvc\interfaces\http\mime;
 
 use DateInterval;
 
-/**
- * use phpdocs to narrow the types via a static type checker.  This is just a small subset of the methods that are
- * part of PSR16.
- */
 interface MimeTypesCacheInterface
 {
     /**
-     * @param string $key
-     * @param mixed|null $default
      * @return array<string, MimeTypeInterface>
      */
-    public function get(string $key, mixed $default = null): mixed;
+    public function getMimeTypes(): array;
 
     /**
-     * @param string $key
      * @param array<string, MimeTypeInterface> $value
      * @param int|DateInterval|null $ttl
      * @return bool
      */
-    public function set(string $key, mixed $value, null|int|DateInterval $ttl = null): bool;
+    public function setMimeTypes(array $value, null|int|DateInterval $ttl = null): bool;
 }
