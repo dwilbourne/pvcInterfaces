d6e247a57240e280d67fc7fb42f2a7333e562172
Doug Wilbourne <dougwilbourne@gmail.com>
2025-03-27 17:42:27 -0400

    removed setMimeTypes method from MimeTypesCacheInterface.php

--- src/http/mime/MimeTypesCacheInterface.php
+++ src/http/mime/MimeTypesCacheInterface.php
@@ -2,19 +2,10 @@
 
 namespace pvc\interfaces\http\mime;
 
-use DateInterval;
-
 interface MimeTypesCacheInterface
 {
     /**
      * @return array<string, MimeTypeInterface>
      */
     public function getMimeTypes(): array;
-
-    /**
-     * @param array<string, MimeTypeInterface> $value
-     * @param int|DateInterval|null $ttl
-     * @return bool
-     */
-    public function setMimeTypes(array $value, null|int|DateInterval $ttl = null): bool;
 }
