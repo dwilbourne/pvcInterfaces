125ae971ed46b86a6ec9c5cf6dbabee9df9397fd
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-08 17:01:05 -0400

    added ContentModelInterface::getContentCategories and moved ContentCategory enum into the interfaces library

---
+++ src/html/content_model/ContentCategory.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace pvc\interfaces\html\content_model;
+
+enum ContentCategory
+{
+    case Interactive;
+    case Transparent;
+}
--- src/html/content_model/ContentModelInterface.php
+++ src/html/content_model/ContentModelInterface.php
@@ -4,5 +4,9 @@ namespace pvc\interfaces\html\content_model;
 
 interface ContentModelInterface
 {
+    /**
+     * @return array<ContentCategory>
+     */
+    public function getContentCategories(): array;
     public function canAddChild(ContentModelInterface $child): bool;
 }
