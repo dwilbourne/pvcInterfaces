9de79c2b40c584e025df10390d0c246b4ebc9ecd
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-09 10:08:26 -0400

    added DomNodeInterface::canAcceptChild method

--- src/html/dom/DomNodeInterface.php
+++ src/html/dom/DomNodeInterface.php
@@ -3,8 +3,11 @@
 namespace pvc\interfaces\html\dom;
 
 use pvc\interfaces\html\content_model\ContentModelInterface;
+use pvc\interfaces\struct\dto\DtoInterface;
 
 interface DomNodeInterface
 {
     public function getContentModel(): ContentModelInterface;
+
+    public function canAcceptChild(DtoInterface $dto): bool;
 }
