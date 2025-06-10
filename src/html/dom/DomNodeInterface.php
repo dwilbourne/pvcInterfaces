7b4329c0bb50243e57013e1bcb8f9da1ad8c6d41
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-10 12:22:05 -0400

    simplified DomNodeInterface by removing getters

--- src/html/dom/DomNodeInterface.php
+++ src/html/dom/DomNodeInterface.php
@@ -2,15 +2,9 @@
 
 namespace pvc\interfaces\html\dom;
 
-use pvc\interfaces\html\content_model\ContentModelInterface;
 use pvc\interfaces\struct\dto\DtoInterface;
-use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 interface DomNodeInterface
 {
-    public function getContainingTreeNode(): TreenodeInterface;
-
-    public function getContentModel(): ContentModelInterface;
-
     public function canAcceptChild(DtoInterface $dto): bool;
 }
