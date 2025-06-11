4ee05cc02febb837ca04ec524dd37cd49bbd71f0
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-11 16:33:55 -0400

    added ContentRuleInterface::ruleTest

--- src/html/content_rule/ContentRuleInterface.php
+++ src/html/content_rule/ContentRuleInterface.php
@@ -2,9 +2,14 @@
 
 namespace pvc\interfaces\html\content_rule;
 
+use pvc\interfaces\html\dom\DomNodeInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 interface ContentRuleInterface
 {
+    /**
+     * @param  TreenodeInterface<DomNodeInterface> $node
+     * @return bool
+     */
     public function ruleTest(TreenodeInterface $node): bool;
 }
