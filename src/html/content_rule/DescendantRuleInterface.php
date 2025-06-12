30599f299233c73dac560aa5eef5a8e746d3067a
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-12 10:42:26 -0400

    modified DescendantRuleInterface.php

--- src/html/content_rule/DescendantRuleInterface.php
+++ src/html/content_rule/DescendantRuleInterface.php
@@ -2,15 +2,17 @@
 
 namespace pvc\interfaces\html\content_rule;
 
+use pvc\interfaces\html\dom\DomNodeInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 interface DescendantRuleInterface
 {
     /**
-     * @param  TreenodeInterface $childNode
+     * @param  TreenodeInterface<DomNodeInterface> $node
+     * @param  TreenodeInterface<DomNodeInterface> $descendant
      * @return bool
-     * return false if child node fails the rule test (e.g. cannot be accepted
+     * return false if descendant node fails the rule test (e.g. cannot be accepted
      * into the DOM tree)
      */
-    public function ruleTest(TreenodeInterface $childNode): bool;
+    public function ruleTest(TreenodeInterface $node, TreenodeInterface $descendant): bool;
 }
