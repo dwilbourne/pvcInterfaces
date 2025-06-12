17b6c8da168e402b007b85dd76f50e9fb8eb68b0
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-12 14:16:23 -0400

    added getContentModel method to DomNodeinterface

--- src/html/dom/DomNodeInterface.php
+++ src/html/dom/DomNodeInterface.php
@@ -2,6 +2,7 @@
 
 namespace pvc\interfaces\html\dom;
 
+use pvc\interfaces\html\content_model\ContentModelInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 interface DomNodeInterface
@@ -10,6 +11,8 @@ interface DomNodeInterface
      * @return TreenodeInterface<DomNodeInterface>
      */
     public function getContainingTreenode(): TreenodeInterface;
+
+    public function getContentModel(): ContentModelInterface;
 
     public function canAcceptChild(DomNodeInterface $child): bool;
 
