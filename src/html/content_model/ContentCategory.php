91576977c2095aaad1e279ba434ac3659bc2ecba
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-13 11:10:25 -0400

    added content categories

--- src/html/content_model/ContentCategory.php
+++ src/html/content_model/ContentCategory.php
@@ -6,4 +6,11 @@ enum ContentCategory: int
 {
     case Interactive = 1;
     case Transparent = 2;
+    case Flow = 4;
+    case Phrasing = 8;
+    case Palpable = 16;
+    case Embedded = 32;
+    case Heading = 64;
+    case Sectioning = 128;
+    case Metadata = 256;
 }
