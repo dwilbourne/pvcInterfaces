79e3709e06333e2d8d3a1c44acf816acb93a2793
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-14 14:37:41 -0400

    added parameter to test method of ContentCategoryRuleInterface

--- src/html/rules/ContentCategoryRuleInterface.php
+++ src/html/rules/ContentCategoryRuleInterface.php
@@ -3,11 +3,13 @@
 namespace pvc\interfaces\html\rules;
 
 use pvc\interfaces\html\content_model\ContentCategory;
+use pvc\interfaces\html\content_model\ContentModelInterface;
 
 interface ContentCategoryRuleInterface
 {
     /**
+     * @param ContentModelInterface $contentModel
      * @return array<ContentCategory>
      */
-    public function test(): array;
+    public function test(ContentModelInterface $contentModel): array;
 }
