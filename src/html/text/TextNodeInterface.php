d90a4101f55e8a8cc4a75a89f96b562aa3cad8e9
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-18 17:33:43 -0400

    added setLocale method to TextNodeInterface.php

--- src/html/text/TextNodeInterface.php
+++ src/html/text/TextNodeInterface.php
@@ -3,7 +3,14 @@
 namespace pvc\interfaces\html\text;
 
 use pvc\interfaces\html\dom\DomNodeInterface;
+use pvc\interfaces\intl\LocaleInterface;
 
 interface TextNodeInterface extends DomNodeInterface
 {
+    /**
+     * @param  LocaleInterface  $locale
+     *
+     * @return void
+     */
+    public function setLocale(LocaleInterface $locale): void;
 }
