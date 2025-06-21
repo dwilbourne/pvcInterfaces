3ff34196d0b945a42e761bb336a1a5d3d709336d
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-21 11:14:18 -0400

    removed TreenodeCollectionInterface.php and TreenodeCollectionFactoryInterface.php, simplified TreenodeFactoryInterface.php

--- src/struct/tree/node/TreenodeFactoryInterface.php
+++ src/struct/tree/node/TreenodeFactoryInterface.php
@@ -8,8 +8,6 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\node;
 
-use pvc\interfaces\struct\tree\tree\TreeInterface;
-
 /**
  * Class TreenodeFactoryInterface
  */
@@ -20,27 +18,4 @@ interface TreenodeFactoryInterface
      * @return TreenodeInterface
      */
     public function makeNode(): TreenodeInterface;
-
-    /**
-     * @param TreeInterface $tree
-     * @return void
-     *
-     * TreenodeFactory and Tree have reciprocal pointers and therefore a circular dependency.  The initialize
-     * method is present so that the TreenodeFactory can be created and then the TreeFactory, in the process of
-     * creating the tree, also initializes the TreenodeFactory (e.g. sets the reciprocal pointer)
-     */
-    public function initialize(TreeInterface $tree): void;
-
-    /**
-     * @return bool
-     */
-    public function isInitialized(): bool;
-
-    /**
-     * @return TreenodeCollectionFactoryInterface
-     * this is in here for an edge case where someone is trying to get the siblings of the root node of the tree.
-     * Because root has no siblings (e.g. there is no parent and thus there is no collection of siblings) we need
-     * to be abel to make an empty Collection to return when someone asks for the siblings of the root.
-     */
-    public function getTreenodeCollectionFactory(): TreenodeCollectionFactoryInterface;
 }
