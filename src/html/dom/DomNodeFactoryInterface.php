b0e937abcd2f9f85e9d0541b0cc33866ff9314d2
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-23 17:16:36 -0400

    added DomNodeFactoryInterface.php

---
+++ src/html/dom/DomNodeFactoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pvc\interfaces\html\dom;
+
+use pvc\interfaces\struct\tree\node\TreenodeFactoryInterface;
+
+interface DomNodeFactoryInterface extends TreenodeFactoryInterface
+{
+
+}
