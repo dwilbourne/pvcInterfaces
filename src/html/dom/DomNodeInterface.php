11b21bc9306ff28fca45c2f6397beb764290527d
Doug Wilbourne <dougwilbourne@gmail.com>
2025-06-24 09:21:49 -0400

    still working on Dom interfaces

--- src/html/dom/DomNodeInterface.php
+++ src/html/dom/DomNodeInterface.php
@@ -2,12 +2,11 @@
 
 namespace pvc\interfaces\html\dom;
 
-use pvc\interfaces\html\content_model\ContentModelInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
 interface DomNodeInterface extends TreenodeInterface
 {
-    public function getContentModel(): ContentModelInterface;
+    public function getContainingDomTreenode() : DomTreenodeInterface;
 
     public function canAcceptContent(DomNodeInterface $content): bool;
 
