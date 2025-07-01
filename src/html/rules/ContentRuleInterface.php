3e29cc9caa5854b403bb910a839cf41fa42c13c4
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-01 11:09:20 -0400

    added flexibility to the return type of content rules

--- src/html/rules/ContentRuleInterface.php
+++ src/html/rules/ContentRuleInterface.php
@@ -6,16 +6,11 @@ use pvc\interfaces\html\content_model\ContentModelInterface;
 
 interface ContentRuleInterface
 {
-    public const int IS_PERMITTED = 1;
-    public const int IS_NOT_APPLICABLE = 0;
-    public const int IS_NOT_PERMITTED = -1;
-
-
     /**
      * @param  ContentModelInterface $content
      * @return bool
      *
-     * returns one of the constants defined above.
+     * returns one of the constants defined in ContentModelInterface
      * @return int<-1, 1>
      */
     public function test(ContentModelInterface $content): int;
