1c356f4541306a25c56fd8231138663c5360d628
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-06 10:05:52 -0400

    reorienting Html content rules around DomNodes

--- src/html/rules/ContentRuleInterface.php
+++ src/html/rules/ContentRuleInterface.php
@@ -2,10 +2,16 @@
 
 namespace pvc\interfaces\html\rules;
 
+use pvc\interfaces\html\content_model\ContentModelInterface;
 use pvc\interfaces\html\dom\DomNodeInterface;
 
 interface ContentRuleInterface
 {
+    /**
+     * @return ContentModelInterface
+     */
+    public function getContainingContentModel(): ContentModelInterface;
+
     /**
      * @param  DomNodeInterface $content
      * returns one of the constants defined in ContentModelInterface
