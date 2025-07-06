e317d683a3386884d5f5ec9fe4f82ceb09510760
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-06 10:43:09 -0400

    reorienting Html content rules around DomNodes

--- src/html/content_model/ContentPermission.php
+++ src/html/content_model/ContentPermission.php
@@ -4,8 +4,8 @@ namespace pvc\interfaces\html\content_model;
 
 enum ContentPermission: int
 {
-    case IS_PERMITTED = 1;
-    case IS_NOT_PERMITTED = -1;
+    case GRANTED = 1;
+    case DENIED = -1;
 
     case NOT_APPLICABLE = 0;
 }
