8991dbba6ceb4cf001bdb47bc620dd54b7dbb417
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-06 09:51:23 -0400

    reorienting Html content rules around DomNodes

--- src/html/rules/CategoryRuleInterface.php
+++ src/html/rules/CategoryRuleInterface.php
@@ -3,18 +3,18 @@
 namespace pvc\interfaces\html\rules;
 
 use pvc\interfaces\html\content_model\ContentCategory;
-use pvc\interfaces\html\content_model\ContentModelInterface;
+use pvc\interfaces\html\dom\DomNodeInterface;
 
 interface CategoryRuleInterface
 {
     /**
-     * @param ContentModelInterface $contentModel
+     * @param DomNodeInterface $content
      * @return array<ContentCategory>
      *
      * test method returns a non-empty array of content categories if,
-     * by virtue of its context, this content model belongs to those
+     * by virtue of its context, this node belongs to those
      * categories.  The array is empty if the context does not support the
      * addition of any content categories
      */
-    public function test(ContentModelInterface $contentModel): array;
+    public function test(DomNodeInterface $content): array;
 }
