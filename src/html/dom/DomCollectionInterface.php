f51fb7a74059d3b8d80dd1d6abe8138fad2aa997
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-08 12:52:09 -0400

    changed return type of Tree:getRoot

---
+++ src/html/dom/DomCollectionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pvc\interfaces\html\dom;
+
+use pvc\interfaces\struct\collection\CollectionInterface;
+
+/**
+ * @extends CollectionInterface<DomNodeInterface>
+ */
+interface DomCollectionInterface extends CollectionInterface
+{
+
+}
