350dfbed58cea0a91c6d4a03f7a77e440ac685ad
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-10 13:01:28 -0400

    made DomElementInterface methods hasName, hasAttribute and hasCategory static

--- src/html/dom/DomElementInterface.php
+++ src/html/dom/DomElementInterface.php
@@ -3,6 +3,7 @@
 namespace pvc\interfaces\html\dom;
 
 use pvc\interfaces\html\attribute\AttributeInterface;
+use pvc\interfaces\html\content_model\ContentCategory;
 use pvc\interfaces\html\content_model\ContentModelInterface;
 
 interface DomElementInterface
@@ -18,9 +19,11 @@ interface DomElementInterface
 
     public function getAttribute(string $name): ?AttributeInterface;
 
-    public function hasName(string $name) : bool;
+    public static function hasName(string $name) : bool;
 
-    public function hasAttribute(string $name) : bool;
+    public static function hasAttribute(string $name) : bool;
+
+    public static function hasCategory(ContentCategory $category) : bool;
 
     public function renderFirstVisit(): string;
 
