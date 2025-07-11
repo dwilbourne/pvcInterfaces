0241f11c89af4e450e3adfadb26c14f1c5ec7ab4
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-11 11:09:19 -0400

    simplification

--- src/html/attribute/AttributeInterface.php
+++ src/html/attribute/AttributeInterface.php
@@ -35,7 +35,7 @@ interface AttributeInterface
     /**
      * @return array<ValueType>|ValueType
      */
-    public function getValue(): array|bool|int|string;
+    public function getValue();
 
     /**
      * render
