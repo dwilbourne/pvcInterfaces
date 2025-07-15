62d42ab6ceed1fefb9309dc0d5dc4e31f9171ffe
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-15 15:45:56 -0400

    made generic TreeType and used it in TrenodeFactory and Treenode

--- src/struct/tree/node/TreenodeInterface.php
+++ src/struct/tree/node/TreenodeInterface.php
@@ -65,12 +65,12 @@ interface TreenodeInterface extends NodeVisitableInterface
 
     /**
      * @function setParent sets a reference to the parent of the node.
-     * @param TreenodeType|null $parent
+     * @param ?non-negative-int $parentId
      * parent node must be in the same tree.
      *
      * @return void
      */
-    public function setParent(?TreenodeInterface $parent): void;
+    public function setParent(?int $parentId): void;
 
     /**
      * @return bool
