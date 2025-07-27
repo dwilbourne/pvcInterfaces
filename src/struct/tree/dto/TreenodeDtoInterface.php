9d6b09e25d13efb73b47863f7aa2990f420ee1b6
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-27 11:52:58 -0400

    made generic TreeType and used it in TrenodeFactory and Treenode

--- src/struct/tree/dto/TreenodeDtoInterface.php
+++ src/struct/tree/dto/TreenodeDtoInterface.php
@@ -4,8 +4,18 @@ namespace pvc\interfaces\struct\tree\dto;
 
 interface TreenodeDtoInterface
 {
+    /**
+     * @return non-negative-int
+     */
     public function getNodeId(): int;
 
+    /**
+     * @return non-negative-int|null
+     */
     public function getParentId(): ?int;
 
+    /**
+     * @return non-negative-int
+     */
+    public function getTreeId(): int;
 }
