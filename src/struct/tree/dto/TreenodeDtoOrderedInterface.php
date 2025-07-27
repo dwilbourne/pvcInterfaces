d56d45c83e9fc411137f7f7de327c18d20db90df
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-27 12:19:15 -0400

    made generic TreeType and used it in TrenodeFactory and Treenode

--- src/struct/tree/dto/TreenodeDtoOrderedInterface.php
+++ src/struct/tree/dto/TreenodeDtoOrderedInterface.php
@@ -2,9 +2,6 @@
 
 namespace pvc\interfaces\struct\tree\dto;
 
-/**
- * @phpstan-type TreenodeDtoShape object{nodeId: non-negative-int, parentId: ?non-negative-int, treeId: ?non-negative-int, index:non-negative-int}
- */
 interface TreenodeDtoOrderedInterface extends TreenodeDtoInterface
 {
     public function getIndex(): int;
