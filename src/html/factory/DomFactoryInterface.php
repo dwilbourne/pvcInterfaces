1b6f40672bd1463ce084a0c5abbf8bc498bf60a9
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-28 16:22:42 -0400

    added makeDomTextElement method to DomFactoryInterface.php

--- src/html/factory/DomFactoryInterface.php
+++ src/html/factory/DomFactoryInterface.php
@@ -3,8 +3,11 @@
 namespace pvc\interfaces\html\factory;
 
 use pvc\interfaces\html\dom\DomElementInterface;
+use pvc\interfaces\html\dom\DomTextElementInterface;
 
 interface DomFactoryInterface
 {
     public function makeDomElement(string $elementName): DomElementInterface;
+
+    public function makeDomTextElement(): DomTextElementInterface;
 }
