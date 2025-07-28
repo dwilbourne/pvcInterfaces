6c1408bcb72c7049b0d7ce0fb7bf260cc64654ac
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-28 08:24:31 -0400

    simplify ContentModelInterface.php and DomElementInterface.php

--- src/html/content_model/ContentModelInterface.php
+++ src/html/content_model/ContentModelInterface.php
@@ -2,37 +2,10 @@
 
 namespace pvc\interfaces\html\content_model;
 
-use pvc\interfaces\html\dom\DomElementInterface;
 use pvc\interfaces\html\dom\DomNodeInterface;
 
 interface ContentModelInterface
 {
-
-    public function getDomElement(): DomElementInterface;
-
-    public function getDomNode(): DomNodeInterface;
-
-    /**
-     * @return int
-     * returns a bitmask of the content categories to which this model
-     * belongs.
-     */
-    public function getCategories(): int;
-
-    /**
-     * @param  ContentCategory  $category
-     *
-     * @return bool
-     */
-    public function hasCategory(ContentCategory $category) : bool;
-
-    /**
-     * @param  array<ContentCategory>  $categories
-     *
-     * @return int
-     */
-    public static function coalesceContentCategories(array $categories): int;
-
     /**
      * @param  DomNodeInterface  $content
      *
--- src/html/dom/DomElementInterface.php
+++ src/html/dom/DomElementInterface.php
@@ -3,26 +3,13 @@
 namespace pvc\interfaces\html\dom;
 
 use pvc\interfaces\html\attribute\AttributeInterface;
-use pvc\interfaces\html\content_model\ContentCategory;
 use pvc\interfaces\html\content_model\ContentModelInterface;
 
-interface DomElementInterface
+interface DomElementInterface extends ContentModelInterface
 {
-    public function getDomNode(): DomNodeInterface;
-
-    public function getContentModel(): ContentModelInterface;
-
     public function getName() : string;
 
     public function getAttribute(string $name): ?AttributeInterface;
 
-    public function hasName(string $name) : bool;
-
-    public function hasAttribute(string $name) : bool;
-
-    public function hasCategory(ContentCategory $category) : bool;
-
-    public function renderFirstVisit(): string;
-
-    public function renderLastVisit(): string;
+    public function render(): string;
 }
