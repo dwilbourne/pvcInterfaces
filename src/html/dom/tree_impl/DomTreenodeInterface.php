634c1a4ec0c1ff2a02c4713d1fce043837be34eb
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-31 08:53:15 -0400

    working on DomInterfaces

--- src/html/dom/tree_impl/DomTreenodeInterface.php
+++ src/html/dom/tree_impl/DomTreenodeInterface.php
@@ -2,6 +2,7 @@
 
 namespace pvc\interfaces\html\dom\tree_impl;
 
+use pvc\interfaces\html\dom\DomNodeCollectionInterface;
 use pvc\interfaces\html\dom\DomNodeInterface;
 use pvc\interfaces\struct\tree\node\TreenodeInterface;
 
@@ -11,4 +12,6 @@ use pvc\interfaces\struct\tree\node\TreenodeInterface;
 interface DomTreenodeInterface extends TreenodeInterface
 {
     public function getDomNode(): DomNodeInterface;
+
+    public function getDomNodeCollection(): DomNodeCollectionInterface;
 }
