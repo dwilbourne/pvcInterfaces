a9af7b75bb541ea92e709553777ebadaeb66a83d
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-31 13:45:51 -0400

    added DomNodeCollectionFactoryInterface.php

---
+++ src/html/dom/DomNodeCollectionFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pvc\interfaces\html\dom;
+
+use pvc\interfaces\struct\collection\CollectionInterface;
+
+/**
+ * @extends CollectionInterface<DomNodeInterface, DomNodeCollectionInterface>
+ */
+interface DomNodeCollectionFactoryInterface extends CollectionInterface
+{
+
+}
