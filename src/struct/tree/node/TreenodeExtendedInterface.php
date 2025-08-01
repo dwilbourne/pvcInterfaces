d7ce259a4aefd113f5e6bda8ca53e129ff2b7f73
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-01 12:37:00 -0400

    separated TreenodeInterface.php into 3 separate interfaces to keep interfaces as narrow as possible

--- src/struct/tree/node/TreenodeExtendedInterface.php
+++ src/struct/tree/node/TreenodeExtendedInterface.php
@@ -8,23 +8,18 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\node;
 
-use pvc\interfaces\struct\collection\IndexedElementInterface;
 use pvc\interfaces\struct\tree\dto\TreenodeDtoInterface;
 
 /**
- * Interface TreenodeExtendedInterface adds methods to TreenodeBaseInterface in
- * the following three areas:
+ * Interface TreenodeExtendedInterface adds methods to TreenodeBaseInterface.
  *
- * 1) it provides methods necessary to hydrate itself with nodeid, parentid and
+ * It provides methods necessary to hydrate itself with nodeid, parentid and
  * optionally a treeid from an external data source.  Once hydrated, it can
  * then be added into the tree.
  *
- * 2) it provides getIndex and setIndex methods which allow you to change the
- * ordinal position of the node among its siblings
- *
  * @see CollectionInterface
  */
-interface TreenodeExtendedInterface extends IndexedElementInterface
+interface TreenodeExtendedInterface
 {
     /**
      * hydration methods
@@ -44,22 +39,4 @@ interface TreenodeExtendedInterface extends IndexedElementInterface
      * the parent already exists in the tree.
      */
     public function getParentId(): ?int;
-
-    /**
-     * indexing methods
-     */
-
-    /**
-     * @return non-negative-int
-     * returns the ordinal position of this node in the parent's child collection
-     */
-    public function getIndex(): int;
-
-    /**
-     * @param  non-negative-int  $index
-     *
-     * @return void
-     */
-    public function setIndex(int $index): void;
-
 }
