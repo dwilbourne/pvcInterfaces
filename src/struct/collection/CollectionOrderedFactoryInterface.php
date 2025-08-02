1d8c1e43aa43eb766589f5bea1df31c799b724d8
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-02 09:40:00 -0400

    working on Tree interfaces

---
+++ src/struct/collection/CollectionOrderedFactoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pvc\interfaces\struct\collection;
+
+/**
+ * Class CollectionOrderedFactoryInterface
+ * @template ElementType of IndexedElementInterface
+ * @template CollectionType of CollectionOrderedInterface
+ */
+
+interface CollectionOrderedFactoryInterface
+{
+    /**
+     * makeCollection
+     * @param array<ElementType> $elements
+     * @return CollectionType<ElementType>
+     */
+    public function makeCollection(array $elements): CollectionOrderedInterface;
+
+}
