b77ffc285c81f002307fff3e3536f6b6f3de122f
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-03 09:48:09 -0400

    working on tree interfaces

---
+++ src/struct/tree/node/TreenodeChildCollectionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pvc\interfaces\struct\tree\node;
+
+use pvc\interfaces\struct\collection\CollectionOrderedInterface;
+
+/**
+ * @extends CollectionOrderedInterface<TreenodeInterface>
+ */
+interface TreenodeChildCollectionInterface extends CollectionOrderedInterface
+{
+
+}
---
+++ src/struct/tree/tree/TreenodeCollectionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pvc\interfaces\struct\tree\tree;
+
+use pvc\interfaces\struct\collection\CollectionInterface;
+use pvc\interfaces\struct\tree\node\TreenodeInterface;
+
+/**
+ * @extends CollectionInterface<TreenodeInterface>
+ */
+interface TreenodeCollectionInterface extends CollectionInterface
+{
+
+}
