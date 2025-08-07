55d5c9302b2392255c43c36f78dace27155339f5
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-07 15:35:47 -0400

    updated to DomNodeInterface.php

--- src/html/dom/DomNodeInterface.php
+++ src/html/dom/DomNodeInterface.php
@@ -4,11 +4,21 @@ namespace pvc\interfaces\html\dom;
 
 use pvc\interfaces\html\content_model\ContentModelInterface;
 
+/**
+ * all DomNodes implement ContentModelInterface
+ */
 interface DomNodeInterface extends ContentModelInterface
 {
+    /**
+     * methods identifying the node
+     */
     public function getNodeType(): NodeType;
     public function getName(): string;
 
+
+    /**
+     * tree-related methods
+     */
     public function getParent(): ?DomNodeInterface;
 
     public function getChildren() : DomNodeCollectionInterface;
@@ -16,22 +26,22 @@ interface DomNodeInterface extends ContentModelInterface
     /**
      * @return DomNodeInterface
      */
-    public function getFirstChild();
+    public function getFirstChild(): ?DomNodeInterface;
 
     /**
      * @return DomNodeInterface
      */
-    public function getLastChild();
+    public function getLastChild(): ?DomNodeInterface;
 
     /**
      * @return DomNodeInterface
      */
-    public function getNthChild(int $n);
+    public function getNthChild(int $n): ?DomNodeInterface;
 
     /**
      * @return DomNodeCollectionInterface
      */
-    public function getSiblings();
+    public function getSiblings(): DomNodeCollectionInterface;
 
     public function render(): string;
 }
