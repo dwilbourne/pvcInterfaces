ee23e6050ef9d368b5a49f62e805d556bbf758f6
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-08 11:32:22 -0400

    updates to Dom interfaces

--- src/html/factory/DomNodeFactoryInterface.php
+++ src/html/factory/DomNodeFactoryInterface.php
@@ -5,14 +5,14 @@ namespace pvc\interfaces\html\factory;
 use pvc\interfaces\html\dom\node_types\DomCommentInterface;
 use pvc\interfaces\html\dom\node_types\DomElementInterface;
 use pvc\interfaces\html\dom\node_types\DomTextInterface;
-use pvc\interfaces\intl\LocaleInterface;
+use pvc\interfaces\msg\MsgInterface;
 
 
 interface DomNodeFactoryInterface
 {
     public function makeDomElement(string $elementName): DomElementInterface;
 
-    public function makeDomText(LocaleInterface $locale): DomTextInterface;
+    public function makeDomText(string|MsgInterface $textMsg): DomTextInterface;
 
     public function makeDomComment(string $comment): DomCommentInterface;
 }
