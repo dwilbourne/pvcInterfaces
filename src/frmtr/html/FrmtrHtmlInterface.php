b4760aef7216b99b1345d576e6548f4eaee46991
Doug Wilbourne <dougwilbourne@gmail.com>
2025-08-10 15:23:07 -0400

    updates to Dom interfaces

---
+++ src/frmtr/html/FrmtrHtmlInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pvc\interfaces\frmtr\html;
+
+use pvc\interfaces\frmtr\FrmtrInterface;
+use pvc\interfaces\html\dom\node_types\DomNodeInterface;
+
+/**
+ * @extends FrmtrInterface<DomNodeInterface>
+ */
+interface FrmtrHtmlInterface extends FrmtrInterface
+{
+    /**
+     * format
+     * @param DomNodeInterface $value
+     * @return string
+     */
+    public function format($value): string;
+
+}
