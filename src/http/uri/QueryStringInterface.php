2c45636250a93777f2a060eaa7e2d9b59f2e6f5e
Doug Wilbourne <dougwilbourne@gmail.com>
2025-09-30 14:14:47 -0400

    added UriInterface.php

--- src/http/uri/QueryStringInterface.php
+++ src/http/uri/QueryStringInterface.php
@@ -4,6 +4,13 @@ namespace pvc\interfaces\http\uri;
 
 use League\Uri\Contracts\QueryInterface;
 
+/**
+ * There is no PSR for a querystring interface per se.  There is some manipulation
+ * of querystrings available through the ServerRequestInterface, but it is really
+ * referring to a server-specific environment in the context of a request.
+ *
+ * So... using the League's interface
+ */
 interface QueryStringInterface extends QueryInterface
 {
 
