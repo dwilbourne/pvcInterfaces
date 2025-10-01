deabb38a706b39d5cbc1a7bdf1e6cfd5001575ba
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-01 10:11:43 -0400

    added come comments to QueryStringInterface.php

--- src/http/uri/QueryStringInterface.php
+++ src/http/uri/QueryStringInterface.php
@@ -9,7 +9,12 @@ use League\Uri\Contracts\QueryInterface;
  * of querystrings available through the ServerRequestInterface, but it is really
  * referring to a server-specific environment in the context of a request.
  *
- * So... using the League's interface
+ * So... using the League's interface.
+ *
+ * Guzzle does not have a query object per se.  It can extract the query as a
+ * string from a Uri via its own Uri object.  For requests, there are several
+ * options for injecting query parameters, none of which are object oriented.
+ * @see https://docs.guzzlephp.org/en/stable/quickstart.html#query-string-parameters
  */
 interface QueryStringInterface extends QueryInterface
 {
