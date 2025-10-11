2498907d84fe26d5f93cc4246527cc479e6df19c
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-11 15:32:53 -0400

    added ArrayIteratorFactoryInterface.php

---
+++ src/struct/collection/ArrayIteratorFactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace pvc\interfaces\struct\collection;
+
+use ArrayIterator;
+
+/**
+ * @template KeyType array-key
+ * @template ElementType
+ */
+interface ArrayIteratorFactoryInterface
+{
+    /**
+     * makeIterator
+     * @param array<KeyType, ElementType> $elements
+     * @return ArrayIterator<KeyType, ElementType>
+     */
+    public function makeIterator(array $elements): ArrayIterator;
+}
