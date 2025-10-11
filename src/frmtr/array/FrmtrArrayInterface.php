b4396365e4c31bb07fdb3f9abab670db4b168efe
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-11 11:04:08 -0400

    updated FrmtrArrayInterface.php

--- src/frmtr/array/FrmtrArrayInterface.php
+++ src/frmtr/array/FrmtrArrayInterface.php
@@ -2,19 +2,21 @@
 
 namespace pvc\interfaces\frmtr\array;
 
+use pvc\interfaces\frmtr\FrmtrInterface;
+
 /**
  * converts an array to a string
+ * @extends FrmtrInterface<array>
  */
-
-interface FrmtrArrayInterface
+interface FrmtrArrayInterface extends FrmtrInterface
 {
     /**
      * format
      *
-     * @param array<mixed>  $array
+     * @param array<mixed>  $value
      *
      * @return string
      * flattens (destructures) an array into a string
      */
-    public function format(array $array): string;
+    public function format($value): string;
 }
