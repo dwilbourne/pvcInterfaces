cc6a7799d37038e2f8d861839501ab90919d583e
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-20 10:12:49 -0400

    expanded container definitions to include method calls and whether the definition is a factory or not

--- src/container/ContainerBuilderInterface.php
+++ src/container/ContainerBuilderInterface.php
@@ -5,14 +5,16 @@ namespace pvc\interfaces\container;
 use Psr\Container\ContainerInterface;
 
 /**
- * @phpstan-type DefArray array{'alias':string, 'class-string':string, 'args'?:array<mixed>}
+ * @phpstan-type Args array<string|number>
+ * @phpstan-type ContainerDefArray array{'alias': string, 'resolvableString': string, 'constructorArgs': Args, 'methodCalls': array<string, Args>, 'factory': bool}
+ * @phpstan-type MethodCallArray array{'methodName': string, 'arguments': array<Args>}
  */
 interface ContainerBuilderInterface
 {
     /**
      * build
      *
-     * @param  array<DefArray>  $pvcDefinitions
+     * @param  array<ContainerDefArray>  $pvcDefinitions
      *
      * @return ContainerInterface
      */
