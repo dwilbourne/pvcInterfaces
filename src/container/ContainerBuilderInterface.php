18519b37cf2634170831075e7abfef4abcee3a00
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-20 10:16:03 -0400

    moved array definitions from ContainerBuilderInterface.php to DefinitionInterface.php

--- src/container/ContainerBuilderInterface.php
+++ src/container/ContainerBuilderInterface.php
@@ -5,9 +5,7 @@ namespace pvc\interfaces\container;
 use Psr\Container\ContainerInterface;
 
 /**
- * @phpstan-type Args array<string|number>
- * @phpstan-type ContainerDefArray array{'alias': string, 'resolvableString': string, 'constructorArgs': Args, 'methodCalls': array<string, Args>, 'factory': bool}
- * @phpstan-type MethodCallArray array{'methodName': string, 'arguments': array<Args>}
+ * @phpstan-import-type ContainerDefArray from DefinitionInterface
  */
 interface ContainerBuilderInterface
 {
--- src/container/DefinitionInterface.php
+++ src/container/DefinitionInterface.php
@@ -4,6 +4,10 @@ namespace pvc\interfaces\container;
 
 /**
  * create vendor-neutral definitions to build objects
+ *
+ * @phpstan-type Args array<string|number>
+ * @phpstan-type ContainerDefArray array{'alias': string, 'resolvableString': string, 'constructorArgs': Args, 'methodCalls': array<string, Args>, 'factory': bool}
+ * @phpstan-type MethodCallArray array{'methodName': string, 'arguments': array<Args>}
  */
 interface DefinitionInterface
 {
