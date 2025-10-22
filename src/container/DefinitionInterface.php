ae7171aca3d8b77ac3b8db624eb866a3be478c40
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-22 10:39:05 -0400

    added clarity to the Container->get method

--- src/container/DefinitionInterface.php
+++ src/container/DefinitionInterface.php
@@ -8,8 +8,24 @@ namespace pvc\interfaces\container;
  * @phpstan-type Args array<string|number>
  * @phpstan-type MethodCallArray array{'methodName': string, 'arguments'?: array<Args>}
  *
- * @phpstan-type ContainerDefArray array{'alias'?: string, 'resolvableString': string, 'constructorArgs'?: Args, 'methodCalls'?: array<string, Args>, 'isFactory'?: bool}
+ * @phpstan-type ContainerDefArray array{'alias'?: string, 'resolvableString': string, 'constructorArgs'?: Args, 'methodCalls'?: array<string, Args>}
  *
+ * some container implementations provide a mechanism for creating a new instance of an object each time
+ * it is retrieved from the container.  E.g. the concept of a factory is embedded in the definitions which
+ * fuel the container (see League\Container from the php League)
+ *
+ * Other container implementations do not. (see phhpdi).
+ *
+ * This discrepancy arises because of the PDS-11 specification which says that when you get an object
+ * from the container, it MAY or MAY NOT be the same instance each time.
+ *
+ * In my view, this is a flaw.  The PSR should define a specific behavior.  For example, if
+ * all containers should be able to make a new instance each time, then change the PSR to add
+ * a 'make' method to ContainerInterface.
+ *
+ * But since that is not currently the case, the pvc Container->get method will always return the same instance
+ * of an object because all implementations are capable of that much.  Of course, if that object is
+ * a factory, then you can use that factory in your code to make as many new instances of something else as you want.
  */
 interface DefinitionInterface
 {
