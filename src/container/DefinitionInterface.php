8255cb96e7b406d988df64e16ced6dcda5aff407
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-22 16:06:28 -0400

    fixed MethodCallArray shape

--- src/container/DefinitionInterface.php
+++ src/container/DefinitionInterface.php
@@ -6,7 +6,7 @@ namespace pvc\interfaces\container;
  * create vendor-neutral definitions to build objects
  *
  * @phpstan-type Args array<string|number>
- * @phpstan-type MethodCallArray array{'methodName': string, 'arguments'?: array<Args>}
+ * @phpstan-type MethodCallArray array{'methodName': string, 'arguments'?: Args}
  *
  * @phpstan-type ContainerDefArray array{'alias'?: string, 'resolvableString': string, 'constructorArgs'?: Args, 'methodCalls'?: array<string, Args>}
  *
