272ca0772d5bce289ad47d4d4b5a3cfcb96eaddd
Doug Wilbourne <dougwilbourne@gmail.com>
2025-11-09 09:24:39 -0500

    added getElement method to TreenodeDtoCollectionInterface.php

--- src/struct/tree/dto/TreenodeDtoCollectionInterface.php
+++ src/struct/tree/dto/TreenodeDtoCollectionInterface.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\dto;
 
+use pvc\interfaces\struct\tree\node\TreenodeInterface;
+use pvc\interfaces\struct\tree\tree\TreeInterface;
 use pvc\interfaces\validator\ValTesterInterface;
 
 /**
  * @template NodeIdType of array-key
+ * @template NodeType of TreenodeInterface
+ * @template TreeIdType of array-key
+ * @template TreeType of TreeInterface
  */
 interface TreenodeDtoCollectionInterface
 {
 
     public function isEmpty(): bool;
+
+    /**
+     * getElement
+     * @param NodeIdType $nodeId
+     *
+     * @return TreenodeDtoInterface<NodeIdType, NodeType, TreeIdType, TreeType>
+     */
+    public function getElement($nodeId): TreenodeDtoInterface;
 
     /**
      * findElementKeys
