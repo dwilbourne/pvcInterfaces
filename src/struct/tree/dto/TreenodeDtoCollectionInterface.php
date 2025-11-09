53145b638245af6d907f6f44337bd7a4ceb8e86d
Doug Wilbourne <dougwilbourne@gmail.com>
2025-11-09 07:41:45 -0500

    added isEmpty and filter methods to TreenodeDtoCollectionInterface.php

--- src/struct/tree/dto/TreenodeDtoCollectionInterface.php
+++ src/struct/tree/dto/TreenodeDtoCollectionInterface.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace pvc\interfaces\struct\tree\dto;
 
+use pvc\interfaces\struct\tree\node\TreenodeInterface;
+use pvc\interfaces\struct\tree\tree\TreeInterface;
+
+/**
+ * @template NodeIdType of array-key
+ * @template NodeType of TreenodeInterface
+ * @template TreeIdType of array-key
+ * @template TreeType of TreeInterface
+ */
 interface TreenodeDtoCollectionInterface
 {
+    public function isEmpty(): bool;
 
+    /**
+     * filter
+     * @param  callable  $callback
+     *
+     * @return TreenodeDtoCollectionInterface<NodeIdType, NodeType, TreeIdType, TreeType>
+     */
+    public function filter(callable $callback): TreenodeDtoCollectionInterface;
 }
