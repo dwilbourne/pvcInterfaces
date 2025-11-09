269e16fd1a3d28f621b130bda05279d8998072f9
Doug Wilbourne <dougwilbourne@gmail.com>
2025-11-09 14:31:50 -0500

    added add method to TreenodeDtoCollectionInterface.php

--- src/struct/treesearch/NodeSearchableCollectionInterface.php
+++ src/struct/treesearch/NodeSearchableCollectionInterface.php
@@ -12,4 +12,18 @@ use Iterator;
 interface NodeSearchableCollectionInterface extends Iterator
 {
     public function initialize(): void;
+
+    /**
+     * add
+     * @param  NodeType  $nodeSearchable
+     *
+     * @return void
+     */
+    public function add(NodeSearchableInterface $nodeSearchable): void;
+
+    /**
+     * __toArray
+     * @return array<NodeIdType, NodeType>
+     */
+    public function __toArray(): array;
 }
