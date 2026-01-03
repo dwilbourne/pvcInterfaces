7f1fcb8b0bd1ad25c6d14227ee62b4c0a282e407
Doug Wilbourne <dougwilbourne@gmail.com>
2026-01-03 10:36:59 -0500

    updated getParent signature in NodeVisitableInterface.php to be compatible with signature in TreenodeInterface

--- src/struct/treesearch/NodeVisitableInterface.php
+++ src/struct/treesearch/NodeVisitableInterface.php
@@ -43,7 +43,7 @@ interface NodeVisitableInterface extends NodeSearchableInterface
      * getParent
      * @return NodeType|null
      */
-    public function getParent(): ?NodeVisitableInterface;
+    public function getParent();
 
     /**
      * getChildren
