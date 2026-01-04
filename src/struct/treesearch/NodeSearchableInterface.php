31511199e851fddb13e7577e2dafab2a449df043
Doug Wilbourne <dougwilbourne@gmail.com>
2026-01-04 10:19:13 -0500

    modified return type of NodeSearchableInterface::getChildren

--- src/struct/treesearch/NodeSearchableInterface.php
+++ src/struct/treesearch/NodeSearchableInterface.php
@@ -34,6 +34,7 @@ interface NodeSearchableInterface
     /**
      * getChildren
      * @return NodeSearchableCollectionInterface<NodeIdType, NodeType>
+     * this return type should be compatible with TreenodeInterface::getChildren
      */
     public function getChildren(): NodeSearchableCollectionInterface;
 
