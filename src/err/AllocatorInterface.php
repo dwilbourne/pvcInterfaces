f95f71187eddcd0f7cf41b3c951ec7cc6580748e
Doug Wilbourne <dougwilbourne@gmail.com>
2026-07-03 15:12:02 -0400

    modified parameter to allocate method in AllocatorInterface.php

--- src/err/AllocatorInterface.php
+++ src/err/AllocatorInterface.php
@@ -5,11 +5,11 @@ namespace pvc\interfaces\err;
 interface AllocatorInterface
 {
     /**
-     * @param  array<mixed, int>  $existing
+     * @param  XCodeDataType  $dataType
      * @return int
      *
      * given an array of existing integer values, return a new integer value that does
      * not already exist in the array
      */
-    public function allocate(array $existing): int;
+    public function allocate(XCodeDataType $dataType): int;
 }
