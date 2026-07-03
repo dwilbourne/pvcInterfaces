a18532174d7d98a21117437fe5242cf6d6c7d49d
Doug Wilbourne <dougwilbourne@gmail.com>
2026-07-03 14:46:14 -0400

    added loadNextNumData and saveNextNumData methods to XCodeStorageInterface.php

--- src/err/XCodeStorageInterface.php
+++ src/err/XCodeStorageInterface.php
@@ -16,4 +16,15 @@ interface XCodeStorageInterface
      * @return void
      */
     public function saveXCodeData(XCodeDataType $dataType, array $data): void;
+
+    /**
+     * @return array<string, int>
+     */
+    public function loadNextNumData(): array;
+
+    /**
+     * @param  array<string, int>  $data
+     * @return void
+     */
+    public function saveNextNumData(array $data): void;
 }
